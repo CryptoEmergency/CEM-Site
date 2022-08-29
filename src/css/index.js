69fd668abba824945d2a7fb5153f88d85862243d
EvgeniiMaiorov91 <80813250+EvgeniiMaiorov91@users.noreply.github.com>
2022-08-29 16:39:37 +0300

    .

--- src/css/index.js
+++ src/css/index.js
@@ -1,2 +1,3 @@
 import "@assets/scss/style.scss";
 import "@assets/css/swiper.min.css";
+import "@assets/css/style.css";
--- src/router/navigation/contacts.js
+++ src/router/navigation/contacts.js
@@ -1,57 +1,106 @@
-import {jsx, jsxFrag, setVariable, getVariable, setAction, makeDOM, getStorage} from '@betarost/cemjs'
-import {init as mainHeader} from '@navigation/header/index.js';
-import contacts_vector from '@assets/image/background/contacts_vector.svg'
+import {
+  jsx,
+  jsxFrag,
+  setVariable,
+  getVariable,
+  setAction,
+  makeDOM,
+  getStorage,
+} from "@betarost/cemjs";
+import { init as mainHeader } from "@navigation/header/index.js";
+import contacts_vector from "@assets/image/background/contacts_vector.svg";
 
-const ID = "mainBlock"
-setVariable({header:true});
-setVariable({footer:true});
+const ID = "mainBlock";
+setVariable({ header: true });
+setVariable({ footer: true });
 
+const contactsView = function () {
+  const lang = getVariable("languages")[getStorage("lang")];
 
-const make = function(){
-    const lang = getVariable("languages")[getStorage("lang")];
-    
-    return (
-        <div class="contacts_container">
-            <img class="background_vector" src={contacts_vector}/>
-            <div class="contacts_content">
-                <div class="contacts_form_block">
-                    <div class="contacts_form">
-                        <h4>{lang.h.contact}</h4>
-                        <p>{lang.p.writeUs}</p>
-                    </div>
-
+  return (
+    <div class="contacts_container">
+      <img class="background_vector" src={contacts_vector} />
+      <div class="contacts_content">
+        <div class="contacts_form_block">
+          <div class="contacts_form">
+            <h4>{lang.h.contact}</h4>
+            <p>{lang.p.writeUs}</p>
+            <form id="contactsForm" data-button_id="sendContacts">
+              <input style="display: none;" type="submit" />
+              <div>
+                <label for="">{lang.label.name}</label>
+                <div class="error-div">{lang.error_div.not_empty_input}</div>
+                <div class="contacts_form_name_icon">
+                  <input
+                    id="contacts_name"
+                    placeholder={lang.placeholder.name}
+                    class="contacts_form_name"
+                    type="text"
+                  />
                 </div>
-            </div>
-
-
-
-
+              </div>
+              <div>
+                <label for="">{lang.label.email}</label>
+                <div class="error-div">{lang.error_div.wrong_email}</div>
+                <div class="contacts_form_email_icon">
+                  <input
+                    id="contacts_email"
+                    placeholder={lang.placeholder.email}
+                    class="contacts_form_email"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div>
+                <label for="">{lang.label.message}</label>
+                <div class="error-div">{lang.error_div.not_empty_input}</div>
+                <div>
+                  <textarea
+                    id="contacts_text"
+                    placeholder={lang.placeholder.message}
+                  ></textarea>
+                </div>
+                <div id="sendContacts" data-action="sendContacts">
+                  <a class="btn-contacts">
+                    <span>{lang.button.send}</span>
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="contacts_info">
+            <span class="contact_info_label">{lang.span.adress}:</span>
+            <span class="contact_info_text">{lang.span.addressFull}</span>
+            <span class="contact_info_label">{lang.span.branch}:</span>
+            <span class="contact_info_text">{lang.span.branchAdress}</span>
+            <span class="contact_info_label">E-mail:</span>
+            <a href="contact_us:info@crypto-emergency.com"><span class="contact_info_text">contact_us@crypto-emergency.com</span></a>
+        </div>
+      </div>
     </div>
+  );
+};
 
+const befor = function (dataUrl) {
+  mainHeader(dataUrl);
+};
 
-        
-    )
-}
+const start = function (dataUrl) {
+  console.log("start contact");
+  makeDOM(contactsView(), ID);
+};
 
-const befor = function(dataUrl){
-    mainHeader(dataUrl);
-}
+const after = function (dataUrl) {};
 
-const start = function(dataUrl){
-    console.log("start contact");
-    makeDOM(make(),ID)
-}
+setAction(ID, "befor", befor);
+setAction(ID, "start", start);
+setAction(ID, "after", after);
 
-const after = function(dataUrl){}
-
-setAction(ID,"befor",befor)
-setAction(ID,"start",start)
-setAction(ID,"after",after)
-
-const init = function(dataUrl){
-    befor(dataUrl)
-    start(dataUrl)
-    after(dataUrl)
-}
+const init = function (dataUrl) {
+  befor(dataUrl);
+  start(dataUrl);
+  after(dataUrl);
+};
 
 export default init;
