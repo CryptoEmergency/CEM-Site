61efbc7895b6124e01b9ea65777edc168b737eed
Betarost <betarost@yandex.ru>
2022-09-01 18:36:40 +0300

    .

--- src/navigation/error404.js
+++ src/navigation/error404.js
@@ -6,7 +6,7 @@ import {
     getStorage,
     setValue
   } from "@betarost/cemjs";
-
+  import { siteLink } from '@src/functions.js'
   import svg from "@assets/svg/index.js";
 
   
@@ -18,7 +18,7 @@ import {
         <h1>{lang.h.notFound}</h1>
         <img   src={svg["icon/error_404.svg"]}/>
         <p>{lang.p.returnToMainPage}</p>
-        <a data-action="link" href="/">
+        <a data-action="link" href="/" onclick={siteLink}>
             <div class="go_home_button_container">
                 <div class="go_home_button">
                     {lang.button.main_page}
