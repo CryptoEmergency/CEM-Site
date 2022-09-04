9b569f03a1c17add4eadb29e87961e9c32694c1f
Betarost <betarost@yandex.ru>
2022-09-05 00:13:41 +0300

    .

--- src/navigation/error404.js
+++ src/navigation/error404.js
@@ -1,40 +1,40 @@
 import {
-    jsx,
-    jsxFrag,
-    getVariable,
-    makeDOM,
-    getStorage,
-    setValue
-  } from "@betarost/cemjs";
-  import { siteLink } from '@src/functions.js'
-  import svg from "@assets/svg/index.js";
+  jsx,
+  jsxFrag,
+  getVariable,
+  makeDOM,
+  getStorage,
+  setValue
+} from "@betarost/cemjs";
+import { siteLink } from '@src/functions.js'
+import svg from "@assets/svg/index.js";
 
-  
-  const view404 = function () {
-    const lang = getVariable("languages")[getStorage("lang")];
-  
-    return (
-        <div class="error_page">
-        <h1>{lang.h.notFound}</h1>
-        <img   src={svg["icon/error_404.svg"]}/>
-        <p>{lang.p.returnToMainPage}</p>
-        <a data-action="link" href="/" onclick={siteLink}>
-            <div class="go_home_button_container">
-                <div class="go_home_button">
-                    {lang.button.main_page}
-                </div>
-            </div>
-        </a>
+
+const view404 = function () {
+  const lang = getVariable("languages")[getStorage("lang")];
+
+  return (
+    <div class="error_page">
+      <h1>{lang.h.notFound}</h1>
+      <img src={svg["icon/error_404.svg"]} />
+      <p>{lang.p.returnToMainPage}</p>
+      <a data-action="link" href="/" onclick={siteLink}>
+        <div class="go_home_button_container">
+          <div class="go_home_button">
+            {lang.button.main_page}
+          </div>
+        </div>
+      </a>
     </div>
-    );
-  };
-  
-  const ID = "mainBlock";
-  
-  const init = function (reload) {
-    setValue("mainHeader", "show", true);
-    setValue("mainFooter", "show", true);
-    makeDOM(view404(), ID);
-  };
-  
-  export default init;
+  );
+};
+
+const ID = "mainBlock";
+
+const init = function (reload) {
+  setValue("mainHeader", "show", true);
+  setValue("mainFooter", "show", true);
+  makeDOM(view404(), ID);
+};
+
+export default init;
