68829a80b5412b148796f99736ddb1a1e1ad44fc
Betarost <betarost@yandex.ru>
2022-09-13 10:02:33 +0300

    .

--- src/component/element/Avatar.js
+++ src/component/element/Avatar.js
@@ -12,7 +12,7 @@ import images from "@assets/images/index.js";
 import svg from "@assets/svg/index.js";
 
 const Avatar = function ({ lang, author, parent = null }) {
-    console.log("Avatar = ", lang, author);
+    //console.log("Avatar = ", lang, author);
 
     return (
         <a href={`${parent != "big_user_avatar" ? `/user/${author.nickname}` : ''}`} class={`${parent == "big_user_avatar" ? "" : "comment_avatar"}`}>
@@ -25,7 +25,7 @@ const Avatar = function ({ lang, author, parent = null }) {
                 <img
                     class="c-avataricon__frame"
                     // style="position: absolute; top: 0;left: 50%;transform: translateX(-50%);z-index: 2; height: 100%;width: "
-                    src={author.frame ? images[`profile/frame/${author.frame.name.split(".")[0]}`] : svg["profile/frame/default"]}
+                    src={author.frame && author.frame.name ? images[`profile/frame/${author.frame.name.split(".")[0]}`] : svg["profile/frame/default"]}
                 />
                 <div class={`c-avataricon__level ${parent == "big_user_avatar" ? "dn" : "user_avatar_level"}`}>
                     <img src={svg.levelGray} />
