f2c3685d1c0a80fcb3a494002b84c9da8414fd1a
Betarost <betarost@yandex.ru>
2022-10-03 23:50:12 +0300

    .

--- src/navigation/header/index.js
+++ src/navigation/header/index.js
@@ -2,166 +2,27 @@ import {
     jsx,
     jsxFrag,
     init,
-    getValue,
-    setValue,
     Variable,
     parsingUrl
 } from '@betarost/cemjs'
 import svg from "@assets/svg/index.js"
-import images from '@assets/images/index.js';
-//import HeaderEmpty from './HeaderEmpty.js'
-//import HeaderNotAuth from './HeaderNotAuth.js'
-//import HeaderAuth from './HeaderAuth.js'
-import HeaderUser from './HeaderUser.js';
+
 import { If } from '@component/helpers/All.js'
-import { clickCancel, siteLink, changeLang } from '@src/functions.js'
+import { siteLink } from '@src/functions.js'
 import { Avatar } from '@component/element/Avatar.js';
 
-const showListLang = function (e) {
-    Variable.langListShow = !Variable.langListShow
-    e.stopPropagation()
-}
-
-const showModalAuth = function (e) {
-    Variable.authModalShow = !Variable.authModalShow;
-    e.stopPropagation();
-    // setValue("modals", "authModalShow", !getValue("modals", "authModalShow"))
-}
-
-const showModalRegistr = function (e) {
-    e.preventDefault()
-    e.stopPropagation()
-    setValue("modals", "registrationModalShow", !getValue("modals", "registrationModalShow"));
-};
-
-const LanguagesList = function (languages) {
-
-    const listLang = Object.keys(languages).map(function (key) {
-        return (
-            <li class="c-changelanguage__item">
-                <a class="c-changelanguage__link" href={"/" + key + "/" + Variable.dataUrl.adress} onclick={changeLang}><span class="c-changelanguage__text">{languages[key].lang_orig}</span></a>
-            </li>
-        )
+const findUnread = function (arr) {
+    let unread = false
+    if (arr == undefined) {
+        return
+    }
+    arr.forEach(element => {
+        if (!element.read) {
+            unread = true
+        }
     })
-
-    return (
-        <ul class="c-changelanguage__list" >
-            {listLang}
-        </ul>
-    )
-
-}
-
-const HeaderAuth = () => (
-    <div class="c-header__container c-container">
-        <div class="c-header__inner">
-            <div class="c-header__auth">
-                <div
-                    class="language"
-                    onclick={showListLang}
-                >
-                    <div class="selectlink">
-                        <div class="selectlink-control"><span>{Variable.lang.lang_orig}</span></div>
-                    </div>
-                </div>
-                <div
-                    class={`c-changelanguage ${Variable.langListShow ? '' : 'dn'}`}
-                    id="listLanguage"
-                    onclick={clickCancel}>
-                    <div class="c-changelanguage__header">
-                        <h4 class="c-changelanguage__title">{Variable.lang.h.modal_listLanguage}</h4>
-                    </div>
-                    {LanguagesList(Variable.languages)}
-                </div>
-                <div class="header_avatar_container">
-
-
-                    {/* <Avatar author={Variable.myInfo} /> */}
-                </div>
-                <div class="auth_user_header">
-                    <div class="c-header__notifications c-notification c-notification--active">
-                        <a class="c-notification__link"></a>
-                        <div class="c-notification__new"></div>
-                    </div>
-                    <div class="c-header__messages c-messages">
-                        <a href="/user/chats/" class="c-messages__link" onclick={siteLink}>
-                            <i class="c-messages__icon"></i>
-                            <div style="display: none;" class="c-messages__counter"></div>
-                        </a>
-                        <div class="c-messages__new"></div>
-                    </div>
-                    <i class="c-header__burger"></i>
-                </div>
-                <div style="display: none;" class="user_notifications_block auth_notifications" id="notifications_block">
-
-                </div>
-            </div>
-            <nav class="c-header__menu c-menu">
-                <a class="c-logo c-menu__link" href="/" onclick={siteLink}>
-                    <img class="c-logo__image" src={svg.logo} />
-                </a>
-                <a class="c-menu__link" href="/contacts/" onclick={siteLink}>{Variable.lang.a.contacts}</a>
-                <a class="c-menu__link" href="/about/" onclick={siteLink}>{Variable.lang.a.about}</a>
-                <a class="c-menu__link" href="/blog/" onclick={siteLink}>{Variable.lang.a.blog}</a>
-            </nav>
-        </div>
-    </div>
-)
-
-
-
-const HeaderNotAuth = () => (
-    <div class="c-header__container c-container">
-        <div class="c-header__inner">
-            <div class="c-header__auth">
-                <div
-                    class="language"
-                    onclick={showListLang}
-                >
-                    <div class="selectlink">
-                        <div class="selectlink-control"><span>{Variable.lang.lang_orig}</span></div>
-                    </div>
-                </div>
-                <div
-                    class={`c-changelanguage ${Variable.langListShow ? '' : 'dn'}`}
-                    id="listLanguage"
-                    onclick={clickCancel}>
-                    <div class="c-changelanguage__header">
-                        <h4 class="c-changelanguage__title">{Variable.lang.h.modal_listLanguage}</h4>
-                    </div>
-                    {LanguagesList(Variable.languages)}
-                </div>
-                <a
-                    class="log-in"
-                    onclick={() => {
-                        Variable.SetModals({ name: "ModalAuth", data: {} })
-                    }}
-                >
-                    {Variable.lang.button.login}
-                </a>
-                <button
-                    class="c-button c-button--gradient"
-                    type="button"
-                    id="registration"
-                    onclick={() => {
-                        Variable.SetModals({ name: "ModalReg", data: {} })
-                    }}
-                >
-                    <span class="c-button__text">{Variable.lang.button.registration}</span>
-                </button>
-            </div>
-            <nav class="c-header__menu c-menu">
-                <a class="c-logo c-menu__link" href="/" onclick={siteLink}>
-                    <img class="c-logo__image" src={svg.logo} />
-                </a>
-                <a class="c-menu__link" href="/contacts/" onclick={siteLink}>{Variable.lang.a.contacts}</a>
-                <a class="c-menu__link" href="/about/" onclick={siteLink}>{Variable.lang.a.about}</a>
-                <a class="c-menu__link" href="/blog/" onclick={siteLink}>{Variable.lang.a.blog}</a>
-            </nav>
-        </div>
-    </div>
-)
-
+    return unread
+};
 
 const mainHeader = async function () {
 
@@ -288,7 +149,134 @@ const mainHeader = async function () {
 
             } else if (Variable.showUserMenu) {
                 return (
-                    <HeaderUser />
+                    <div class="c-header__userpanel">
+                        <div class="c-userpanel c-userpanel--left">
+                            <div class="c-userpanel__icons">
+                                <a
+                                    class="c-userpanel__icon c-userpanel__icon--logo"
+                                    href="/"
+                                    onclick={siteLink}>
+                                    <img width="45" height="39" src={svg["mini_logo"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.home}</div>
+                                </a>
+                                <If
+                                    data={Variable.auth}
+                                    dataIf={<Avatar author={Variable.myInfo} parent={'c-userpanel__icon--avatar'} />}
+                                />
+                                <a
+                                    class={`c-userpanel__icon c-userpanel__icon--only_mobile_visible ${(Variable.dataUrl.adress == "user" && !Variable.dataUrl.category) ? "c-userpanel__icon--active" : ""}`}
+                                    href="/user/"
+                                    onclick={siteLink}
+                                >
+                                    <img src={svg["profile_icon-1"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.profile}</div>
+                                </a>
+                                <a
+                                    class={`c-userpanel__icon c-userpanel__icon--mobile_visible ${Variable.dataUrl.adress == "lenta-users" ? "c-userpanel__icon--active" : ""}`}
+                                    href="/lenta-users/"
+                                    onclick={siteLink}
+                                >
+                                    <img src={svg.user_news_page} />
+                                    <div class="c-userpanel__description">{Variable.lang.span.userNews}</div>
+                                </a>
+                                <a
+                                    onclick={siteLink}
+                                    class="c-userpanel__icon c-userpanel__icon--mobile_visible"
+                                >
+                                    <img src={svg.plus_in_circle} />
+                                </a>
+                                <a
+                                    onclick={siteLink}
+                                    class={`c-userpanel__icon c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "chats") ? "c-userpanel__icon--active" : ""}`}
+                                    href="/user/chats/"
+                                >
+                                    <img src={svg["profile_icon-2"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.chats}</div>
+                                    <div style="display: none;" class="c-userpanel__counter"></div>
+                                </a>
+                                <a
+                                    onclick={siteLink}
+                                    class={`c-userpanel__icon ${(Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "awards") ? "c-userpanel__icon--active" : ""}`}
+                                    href="/user/awards/"
+                                >
+                                    <img src={svg["profile_icon-4"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.awards}</div>
+                                </a>
+                                <a
+                                    onclick={siteLink}
+                                    class={`c-userpanel__icon  ${(Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "quests") ? "c-userpanel__icon--active" : ""}`}
+                                    href="/user/quests/"
+                                >
+                                    <img src={svg["profile_icon-10"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.tasks}</div>
+                                </a>
+                                <a
+                                    style="margin-bottom: 15px;"
+                                    class="c-userpanel__icon c-userpanel__icon--notification c-userpanel__icon--mobile_visible user_panel_notify"
+                                    onclick={siteLink}
+                                >
+                                    <img
+                                        style={`${(findUnread(Variable.notifyQuestions) || findUnread(Variable.notifyAwards) || findUnread(Variable.notifySystem)) ? "filter: invert(61%) sepia(60%) saturate(485%) hue-rotate(94deg) brightness(94%) contrast(96%)" : ""}`}
+                                        src={svg.notifications_icon}
+                                    />
+                                </a>
+                                <a
+                                    onclick={siteLink}
+                                    class={`c-userpanel__icon  ${(Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "wallet") ? "c-userpanel__icon--active" : ""}`}
+                                    href="/user/wallet/"
+                                >
+                                    <img src={svg.absolutely_new_wallet} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.wallet}</div>
+                                </a>
+                                <a
+                                    onclick={siteLink}
+                                    class={`c-userpanel__icon  ${(Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "affiliate") ? "c-userpanel__icon--active" : ""}`}
+                                    href="/user/affiliate/"
+                                >
+                                    <img src={svg["profile_icon-3"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.affiliate}</div>
+                                </a>
+                                <If
+                                    data={Variable.auth}
+                                    dataIf={<a class="c-userpanel__icon" href="/logout/" onclick={siteLink}>
+                                        <img src={svg["exit-icon"]} />
+                                        <div class="c-userpanel__description">{Variable.lang.a.exit}</div>
+                                    </a>}
+                                />
+                                <a
+                                    id="mobileUserMenuShow"
+                                    class="c-userpanel__icon c-userpanel__icon--mobile_visible c-userpanel__icon--burger"
+                                >
+                                    <img src={svg.user_burger_menu} />
+                                </a>
+                            </div>
+                            <div class="c-userpanel__addmodal">
+                                <div class="c-userpanel__inner">
+                                    <p>{Variable.lang.button.create}</p>
+                                    <a class="c-userpanel__link" onclick={siteLink} href="/user/posts/">
+                                        <img src={svg["profile_icon-5"]} />
+                                        {Variable.lang.h.createPost}
+                                    </a>
+                                </div>
+                                <div data-action="user_cabinet_add_close" class="c-userpanel__close">
+                                    <img src={svg.close} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="c-userpanel c-userpanel--right">
+                            <div class="c-userpanel__icons">
+                                <a onclick={siteLink} class="c-userpanel__icon" href="/users/">
+                                    <img src={svg["profile_icon-1"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.experts}</div>
+                                </a>
+                                <a onclick={siteLink} class="c-userpanel__icon" href="/question/">
+                                    <img src={svg["user_mobile_answers_and_questions"]} />
+                                    <div class="c-userpanel__description">{Variable.lang.a.questionsAnswers}</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div >
                 )
             } else {
                 return (
