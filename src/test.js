d98870218ad5af1e5a85eaef24e5113665aba807
Betarost <betarost@yandex.ru>
2022-10-13 01:18:52 +0300

    .

--- src/test.js
+++ src/test.js
@@ -1,10 +1,18 @@
 import {
   Variable,
   getInitList,
-  getTimerInit
+  getTimerInit,
+  initScreen
 } from '@betarost/cemjs'
 
+import { mainHeader } from "@navigation/header/index.js";
+import { mainFooter } from '@navigation/footer/index.js';
+
+
 const test = async function () {
+
+  // initScreen([{ name: "Header", elem: null, run: mainHeader }, { name: "Footer", elem: null, run: mainFooter }]);
+
   setInterval(async () => {
     console.log("=======================")
     console.log("Дебаг каждые 60 секунд:")
