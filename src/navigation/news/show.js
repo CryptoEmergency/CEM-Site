7fa055095b7788601997e4a34fdb86a9e046e235
Betarost <betarost@yandex.ru>
2022-10-28 14:43:58 +0300

    .

--- src/navigation/news/show.js
+++ src/navigation/news/show.js
@@ -7,16 +7,20 @@ import {
 } from "@betarost/cemjs";
 import { BlockNewsShow } from '@component/blocks/index.js';
 
-const start = function () {
+const start = function (data, ID = "mainBlock") {
   let item;
   Variable.HeaderShow = true;
   Variable.FooterShow = true;
-
+  console.log('=5c27ea= show', item)
   init(
     async () => {
-      item = await sendApi.send({ action: "getNews", short: true, limit: 1, filter: { _id: Variable.dataUrl.params } });
+
+      item = await sendApi.send({ action: "getNews", short: true, limit: 1, filter: { _id: Variable.Static.DataUrl.params } });
+      console.log('=5c27ea= init1', item, Variable.Static)
+
     },
     () => {
+      console.log('=5c27ea= init2', item)
       return (
         <div class={[Variable.HeaderShow ? 'c-main__body' : 'c-main__body--noheader']}>
           <div class="full_news_container">
@@ -31,7 +35,7 @@ const start = function () {
           </div>
         </div>
       );
-    }
+    }, ID
   );
 };
 //I check
