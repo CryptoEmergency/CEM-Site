6a773b183470f0b6fe3f7b8e6370a5df92cc981a
Betarost <betarost@yandex.ru>
2022-11-02 18:07:34 +0300

    .

--- src/component/element/Input.js
+++ src/component/element/Input.js
@@ -1,7 +1,11 @@
-import { jsx, jsxFrag, Helpers, Variable } from "@betarost/cemjs";
+import { jsx, jsxFrag, Helpers, Variable, initReload } from "@betarost/cemjs";
+import svg from '@assets/svg/index.js';
 
+let viewPassword = false
 
-const Input = function ({ label, error, placeholder, type, value, condition, Static, afterValid, className }) {
+let timer
+
+const Input = function ({ label, error, placeholder, type, value, condition, Static, afterValid, className, dopBefor }) {
 
     return (
         <div>
@@ -18,27 +22,55 @@ const Input = function ({ label, error, placeholder, type, value, condition, Sta
                 }
             </div>
             <div class={className}>
+                {() => {
+                    if (type == "password") {
+                        return (
+                            <img src={svg.lock} class="icon-input" />
+                        )
+                    }
+                }}
+                {dopBefor}
                 <input
                     placeholder={placeholder}
-                    type={type}
+                    type={viewPassword ? "text" : type}
                     value={value}
                     oninput={function (e) {
-                        Static.value = this.value.trim()
-                        if (!condition) {
-                            return
+                        if (timer) {
+                            clearTimeout(timer);
                         }
-                        Static.valid = condition(this.value.trim())
-                        Static.error = !Static.valid
-                        if (Static.error) {
-                            this.style = "border-color: rgb(200, 23, 38);";
-                        } else {
-                            this.style = "border-color: rgb(37, 249, 48);"
-                        }
-                        if (afterValid) {
-                            afterValid();
-                        }
+                        timer = setTimeout(() => {
+                            timer = null
+                            Static.value = this.value.trim()
+                            if (!condition) {
+                                return
+                            }
+                            Static.valid = condition(this.value.trim())
+                            Static.error = !Static.valid
+                            if (Static.error) {
+                                this.style = "border-color: rgb(200, 23, 38);";
+                            } else {
+                                this.style = "border-color: rgb(37, 249, 48);"
+                            }
+                            if (afterValid) {
+                                afterValid();
+                            }
+                        }, 500);
                     }}
                 />
+                {() => {
+                    if (type == "password") {
+                        return (
+                            <img
+                                src={svg[`eye${viewPassword ? '-slash' : ''}`]}
+                                class="password_eye"
+                                onClick={() => {
+                                    viewPassword = !viewPassword
+                                    initReload()
+                                }}
+                            />
+                        )
+                    }
+                }}
             </div>
         </div>
     );
