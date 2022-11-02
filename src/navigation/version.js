5f7faf6b1fe9b35b42f9d514074562a61f1300a4
sivwallin <n0name-lnt@yandex.ru>
2022-11-02 15:29:19 +0300

    ^^

--- src/navigation/version.js
+++ src/navigation/version.js
@@ -9,30 +9,21 @@ import {
 
 import { api } from '@src/apiFunctions.js'
 
+import svg from "@assets/svg/index.js";
+
 const start = function (data, ID = "mainBlock") {
 
     Variable.Static.tpm = 5
-    console.log(Variable)
-
-
     init(
         async () => {
 
-            //await api({ type: "get", action: "getNews", short: true, cache: true, name: "PageNews", filter: { type: "news" } })
-            await api({ type: "get", action: "getNews", short: true, name: "TEST", filter: { type: "news" } })
-            console.log('=69a2cd=', Variable.TEST)
-            setTimeout(() => {
-                Variable.Static.tpm = 7
-
-                // initReload()
-            }, 2500);
         },
         () => {
-            console.log('=2d7519=', "2 функция")
             return (
                 <div class="c-main__body">
                     {/* <div class={[Variable.HeaderShow ? 'c-main__body' : 'c-main__body--noheader']}> */}
                     Version page {Variable.Static.tpm}
+                    <img src={svg['load']}/>
                 </div>
             )
         }, ID)
