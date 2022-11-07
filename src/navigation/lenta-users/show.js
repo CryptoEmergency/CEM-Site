ef0a709264ff0a33b71ed3d848c453dfec2f558f
Betarost <betarost@yandex.ru>
2022-11-07 12:26:47 +0300

    .

--- src/navigation/lenta-users/show.js
+++ src/navigation/lenta-users/show.js
@@ -1,44 +1,43 @@
 import {
-    jsx,
-    jsxFrag,
-    Variable,
-    init,
-    sendApi,
-    getStorage,
-    initReload
-  } from "@betarost/cemjs";
-  
+  jsx,
+  jsxFrag,
+  Variable,
+  init,
+  sendApi,
+  getStorage,
+  initReload
+} from "@betarost/cemjs";
 
-  import { api } from '@src/apiFunctions.js'
-  import { BlockUserForLenta } from "@component/blocks/index.js";
 
-  const start = function (data, ID = "mainBlock") {
-    let item;
+import { api } from '@src/apiFunctions.js'
+import { BlockShowLenta } from "@component/blocks/index.js";
 
-  
-    init(
+const start = function (data, ID = "mainBlock") {
+  let item;
 
-        
-      async () => {
-        if (data && data.item) {
-          item = data.item
-        } else {
-          let response = await api({ type: "get", action: "getPost", short: true, limit: 1, filter: { _id: Variable.dataUrl.params } })
-          item = response.list_records[0]
-        }
-      },
-      async () => {
-  
-        return (
-  
-          <div class="answer_container c-main__body">
-  <BlockUserForLenta item={item} />
-       
-          </div>
-        )
-      }, ID
-    );
-  };
-  
-  export default start;
-  
+
+  init(
+
+
+    async () => {
+      if (data && data.item) {
+        item = data.item
+      } else {
+        let response = await api({ type: "get", action: "getPost", short: true, limit: 1, filter: { _id: Variable.dataUrl.params } })
+        item = response.list_records[0]
+      }
+    },
+    async () => {
+
+      return (
+
+        <div class="answer_container c-main__body">
+          <BlockShowLenta item={item} />
+
+        </div>
+      )
+    }, ID
+  );
+};
+
+export default start;
