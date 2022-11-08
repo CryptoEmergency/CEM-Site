2961023f7f03fa79770190974f0fae45ec71aad5
Betarost <betarost@yandex.ru>
2022-11-08 23:17:31 +0300

    .

--- src/component/element/index.js
+++ src/component/element/index.js
@@ -19,8 +19,7 @@ export { MediaPreview } from "./MediaPreview.js"
 export { NotFound } from "./NotFound.js"
 // Свайпер
 export { Swiper } from "./Swiper.js";
-// Плееры
-export { VideoPlayer } from "./player/VideoPlayer.js";
+
 
 //===formsElement===
 // Формы ввода: Инпуты, чекбоксы
@@ -29,12 +28,6 @@ export { TextArea } from "./formsElement/TextArea.js";
 export { ButtonSubmit } from "./formsElement/ButtonSubmit.js"
 export { ButtonShowMore } from "./formsElement/ButtonShowMore.js"
 export { Select } from "./formsElement/Select.js"
-// Меню три точки
-export { ItemsMenu } from './ItemsMenu.js';
-
-//
-export { UserWalletCard } from './user/UserWalletCard.js';
-export { Avatar } from './Avatar.js';
 export { CheckBox } from "./formsElement/CheckBox.js";
 //============
 
@@ -46,18 +39,6 @@ export { AudioPlayerCopy } from "./AudioPlayerCopy.js"
 export { AudioPlayer } from "./player/AudioPlayer.js"
 //============
 
-export { MediaButton } from "./MediaButton.js"
-
-
-export { MediaPreview } from "./MediaPreview.js"
-
-
-export { CheckBox } from "./CheckBox.js";
-
-
-export { Comment } from "./Comment.js";
-export { Evaluation } from "./Evaluation.js";
-export { NotFound } from "./NotFound.js"
 
 //===user===
 // Отображение клшелька (счета)
