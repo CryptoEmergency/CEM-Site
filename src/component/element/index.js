a15d99fcf2104c94317a33965fb373aaa2ce9a42
Betarost <betarost@yandex.ru>
2022-11-08 23:13:52 +0300

    .

--- src/component/element/index.js
+++ src/component/element/index.js
@@ -1,7 +1,28 @@
+
+// Отображение аватарки 
+export { Avatar } from './Avatar.js';
+// Нижнее меню полоска
+export { BottomMenu } from './BottomMenu.js';
+// Вывод комментариев 
+export { Comment } from "./Comment.js";
+// Лайки, дизлайки
+export { Evaluation } from "./Evaluation.js";
+// Меню раскрывающиеся три точки
+export { ItemsMenu } from './ItemsMenu.js';
+// Вывод медиа информации в постах
+export { LentaMedia } from "./LentaMedia.js"
+// Кнопки для выбора загрузки медиа
+export { MediaButton } from "./MediaButton.js"
+// Отображение загрузки и миниатюр медиа при загрузке
+export { MediaPreview } from "./MediaPreview.js"
+// Записей не найдено, при поиске и выводе 
+export { NotFound } from "./NotFound.js"
 // Свайпер
 export { Swiper } from "./Swiper.js";
 // Плееры
 export { VideoPlayer } from "./player/VideoPlayer.js";
+
+//===formsElement===
 // Формы ввода: Инпуты, чекбоксы
 export { Input } from "./formsElement/Input.js";
 export { TextArea } from "./formsElement/TextArea.js";
@@ -14,10 +35,16 @@ export { ItemsMenu } from './ItemsMenu.js';
 //
 export { UserWalletCard } from './user/UserWalletCard.js';
 export { Avatar } from './Avatar.js';
+export { CheckBox } from "./formsElement/CheckBox.js";
+//============
 
 export { LentaMedia } from "./LentaMedia.js"
+//===player===
+// Медиа плееры
+export { VideoPlayer } from "./player/VideoPlayer.js";
 export { AudioPlayerCopy } from "./AudioPlayerCopy.js"
 export { AudioPlayer } from "./player/AudioPlayer.js"
+//============
 
 export { MediaButton } from "./MediaButton.js"
 
@@ -32,4 +59,11 @@ export { Comment } from "./Comment.js";
 export { Evaluation } from "./Evaluation.js";
 export { NotFound } from "./NotFound.js"
 
+//===user===
+// Отображение клшелька (счета)
+export { UserWalletCard } from './user/UserWalletCard.js';
+//============
 
+//
+export { NotifyItem } from './NotifyItem.js';
+export { PhoneCode } from './PhoneCode.js';
