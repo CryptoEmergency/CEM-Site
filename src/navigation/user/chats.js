55137633ec84ac5c298cc253b8382d65aa3e34a8
Betarost <betarost@yandex.ru>
2022-11-10 09:21:51 +0300

    .

--- src/navigation/user/chats.js
+++ src/navigation/user/chats.js
@@ -13,6 +13,9 @@ import svg from '@assets/svg/index.js';
 import { Avatar } from '@component/element/index.js';
 
 const start = function () {
+    let Static = {}
+
+
     let chatsList,
         activeChat,
         messageList,
@@ -64,8 +67,8 @@ const start = function () {
                     class={[
                         "messages_block",
                         Variable.Static.HeaderShow ? "c-main__body" : "c-main__body--noheader",
-                      ]}
-                    >
+                    ]}
+                >
                     <div class="messages_left_part">
                         <div class="chats_search">
                         </div>
@@ -119,43 +122,25 @@ const start = function () {
                                                     initReload()
                                                 }}
                                             >
-                                                <Avatar
-                                                    author={user} />
+                                                <Avatar author={user} />
                                                 <div class="messages_list_item_info">
                                                     <div class="messages_list_item_info-1">
                                                         <p>{user.nickname}</p>
-                                                        <If
-                                                            data={lastMessage.text}
-                                                            dataIf={
-                                                                <span>{lastMessage.text}</span>
-                                                            }
-                                                        />
-
+                                                        {lastMessage.text ? <span>{lastMessage.text}</span> : null}
                                                     </div>
                                                     <div class="messages_list_item_info-2">
-                                                        <If
-                                                            data={lastMessage.author == Variable.myInfo._id}
-                                                            dataIf={
-                                                                <p>{Helpers.getDateFormat(lastMessage.showDate)}</p>
-                                                            }
-                                                            dataElse={
-                                                                <p class="message--new">
-                                                                    <span>{Helpers.getDateFormat(lastMessage.showDate)}</span>
-                                                                    <If
-                                                                        data={item.unreadMessage}
-                                                                        dataIf={<i>{item.unreadMessage}</i>}
-                                                                    />
-                                                                </p>
-                                                            }
-                                                        />
+                                                        {lastMessage.author == Variable.myInfo._id
+                                                            ?
+                                                            <p>{Helpers.getDateFormat(lastMessage.showDate, "now")}</p>
+                                                            :
+                                                            <p class="message--new">
+                                                                <span>{Helpers.getDateFormat(lastMessage.showDate, "now")}</span>
+                                                                {item.unreadMessage ? <i>{item.unreadMessage}</i> : null}
 
-                                                        <If
-                                                            data={lastMessage.author == Variable.myInfo._id}
-                                                            dataIf={
-                                                                <img src={svg[iconStatus]} />
-                                                            }
-                                                        />
+                                                            </p>
+                                                        }
 
+                                                        {lastMessage.author == Variable.myInfo._id ? <img src={svg[iconStatus]} /> : null}
                                                     </div>
                                                 </div>
                                             </div>
@@ -164,99 +149,6 @@ const start = function () {
                                     return (arrReturn)
                                 }
                             }}
-
-                            <Map
-                                data={chatsList.list_records}
-                                dataIf={(item, index) => {
-                                    let user
-                                    let lastMessage = item.message[0]
-                                    let iconStatus
-
-                                    if (lastMessage.status == 0) {
-                                        iconStatus = "sent_message_icon"
-                                    } else if (lastMessage.status == 1) {
-                                        iconStatus = "unread_message_icon"
-                                    } else {
-                                        iconStatus = "read_message_icon"
-                                    }
-
-                                    if (Variable.myInfo._id != item.users[0]._id) {
-                                        user = item.users[0]
-                                    } else {
-                                        user = item.users[1]
-                                    }
-                                    return (
-                                        <div
-                                            class={["messages_list_item", item._id == activeChat ? "active" : null]}
-                                            onclick={async () => {
-                                                activeChat = item._id
-                                                messageList = await sendApi.send({
-                                                    action: "getUserChats", short: true,
-                                                    filter: {
-                                                        "$and": [
-                                                            {
-                                                                "users": user._id
-                                                            }
-                                                        ]
-                                                    },
-                                                    select: {
-                                                        "message": {
-                                                            "$slice": [
-                                                                0,
-                                                                120
-                                                            ]
-                                                        },
-                                                        "users": 1
-                                                    }
-                                                });
-                                                console.log('=c35516=', messageList)
-                                                initReload()
-                                            }}
-                                        >
-                                            <Avatar
-                                                author={user} />
-                                            <div class="messages_list_item_info">
-                                                <div class="messages_list_item_info-1">
-                                                    <p>{user.nickname}</p>
-                                                    <If
-                                                        data={lastMessage.text}
-                                                        dataIf={
-                                                            <span>{lastMessage.text}</span>
-                                                        }
-                                                    />
-
-                                                </div>
-                                                <div class="messages_list_item_info-2">
-                                                    <If
-                                                        data={lastMessage.author == Variable.myInfo._id}
-                                                        dataIf={
-                                                            <p>{Helpers.getDateFormat(lastMessage.showDate)}</p>
-                                                        }
-                                                        dataElse={
-                                                            <p class="message--new">
-                                                                <span>{Helpers.getDateFormat(lastMessage.showDate)}</span>
-                                                                <If
-                                                                    data={item.unreadMessage}
-                                                                    dataIf={<i>{item.unreadMessage}</i>}
-                                                                />
-                                                            </p>
-                                                        }
-                                                    />
-
-                                                    <If
-                                                        data={lastMessage.author == Variable.myInfo._id}
-                                                        dataIf={
-                                                            <img src={svg[iconStatus]} />
-                                                        }
-                                                    />
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )
-                                }
-                                }
-                            />
                         </div>
                     </div>
                     <div class="messages_dialog">
@@ -267,12 +159,29 @@ const start = function () {
                                         <div class="companion">
                                             <Avatar author={activeUser} />
                                             <p>{activeUser.nickname}</p>
-                                            {/* <If
-                                            data={Variable.myInfo._id != messageList.list_records[0].users[0]._id}
-                                            dataIf={
-                                                <Avatar author={messageList.list_records[0].users[0]} />
-                                            }
-                                        /> */}
+                                        </div>
+                                        <div class="messages_container">
+                                            {() => {
+                                                if (messageList && messageList.list_records && messageList.list_records[0].message) {
+                                                    const arrReturn = messageList.list_records[0].message.map((item, index) => {
+                                                        console.log('=0a9ec9=', item)
+                                                        return (
+                                                            <div class={item.author == Variable.myInfo._id ? "your_message_container" : "friend_message_container"}>
+                                                                <div class={[item.author == Variable.myInfo._id ? "your_message" : "friend_message", Helpers.ifHaveMedia(item.media, "video") ? "chat_have_video" : null, Helpers.ifHaveMedia(item.media, "audio") ? "chat_have_audio" : null]} >
+                                                                    {Helpers.editText(item.text, { clear: true, paragraph: true, html: true })}
+
+                                                                    <div class={item.author == Variable.myInfo._id ? "your_message_date" : "friend_message_date"}>
+                                                                        {Helpers.getDateFormat(item.showDate, "now")}
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })
+
+                                                    return arrReturn
+                                                }
+                                            }}
+
                                         </div>
                                     </div>
                                 )
