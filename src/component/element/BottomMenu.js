14cd3276ec2a67e9c5efd0f66f77f8ed0213c58b
Betarost <betarost@yandex.ru>
2022-11-14 22:53:17 +0300

    .

--- src/component/element/BottomMenu.js
+++ src/component/element/BottomMenu.js
@@ -1,183 +1,93 @@
 import {
     jsx,
     jsxFrag,
-    Helpers,
     Variable
 } from "@betarost/cemjs";
-import svg from "@assets/svg/index.js";
 import { fn } from "@src/functions/index.js";
 
 const findUnread = function (arr) {
     let unread = false
-    if (arr == undefined) {
-        return
-    }
+    if (!arr || !arr.length) { return false }
     arr.forEach(element => {
-        if (!element.read) {
-            unread = true
-        }
+        if (!element.read) { unread = true }
     })
     return unread
 };
 
 const BottomMenu = function () {
-
-    const toggleVisibleNotify = function () {
-        Variable.notifyWindowShow = !Variable.notifyWindowShow;
-    }
-
-    if (Variable.auth) {
-        return (
-            <div class="c-userpanel c-userpanel--bottom">
-                <div class="c-userpanel__icons">
-                    <a
-                        href="/"
-                        class="c-userpanel__icon c-userpanel__icon--logo c-userpanel__icon--mobile_visible"
-                        onclick={fn.siteLink}>
-                    </a>
-                    <a
-                        href="/lenta-users/"
-                        class={["c-userpanel__icon c-userpanel__icon--lenta c-userpanel__icon--mobile_visible", Variable.dataUrl.adress == "lenta-users" ? "c-userpanel__icon--active" : null]}
-                        onclick={fn.siteLink}>
-
-                    </a>
-                    <a
-                        href="/user/chats/"
-                        class={["c-userpanel__icon c-userpanel__icon--chats c-userpanel__icon--disabled c-userpanel__icon--mobile_visible", (Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "chats") ? "c-userpanel__icon--active" : null]}
-                        onclick={fn.siteLink}>
-                        {() => {
-                            if (Variable.myInfo && Variable.myInfo.unreadMessage) {
-                                return (<div class="messages_notifications_counter">{Variable.myInfo.unreadMessage}</div>)
-                            }
-                        }}
-                    </a>
-
-                    <a
-                        href="/user/posts/"
-                        onclick={Helpers.siteLink}
-                        class="c-userpanel__icon c-userpanel__icon--posts c-userpanel__icon--mobile_visible"
-                    ></a>
-                    <a
-                        onclick={Helpers.siteLink}
-                        class={`c-userpanel__icon c-userpanel__icon--questions c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "question") ? "c-userpanel__icon--active" : ""}`}
-                        href="/question/"
-                    ></a>
-                    {/* <a
-                        onclick={Helpers.siteLink}
-                        class={`c-userpanel__icon c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "news") ? "c-userpanel__icon--active" : ""}`}
-                        href="/news/"
-                    >
-                        <img src={svg.news_menu_icon1} width="42" height="39" />
-                    </a> */}
-
-                    {/* <a
-                        href="/user/notify/"
-                        style="margin-bottom: 15px;"
-                        class={`c-userpanel__icon c-userpanel__icon--notification c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "notify") ? "c-userpanel__icon--active" : ""}`}
-                        onclick={Helpers.siteLink}
-                    >
-                        <img
-                            style={`${(findUnread(Variable.notifyQuestions) || findUnread(Variable.notifyAwards) || findUnread(Variable.notifySystem)) ? "filter: invert(61%) sepia(60%) saturate(485%) hue-rotate(94deg) brightness(94%) contrast(96%)" : ""}`}
-                            src={svg.notifications_icon}
-                            width="27"
-                            height="36"
-                        />
-                    </a> */}
-
-                    <div class={`c-notification ${(findUnread(Variable.notifyQuestions) || findUnread(Variable.notifyAwards) || findUnread(Variable.notifySystem)) ? "c-notification--active" : ""}`}>
-                        <a
-                            class="c-userpanel__icon c-userpanel__icon--notify c-userpanel__icon--mobile_visible c-notification__link"
-                            // onClick={toggleVisibleNotify}
-                            onClick={
-                                (e) => {
-                                    Variable.SetModals({ name: "ModalNotify", data: {} })
-                                    e.stopPropagation();
-                                }
-                            }
-                        ></a>
-                        <div class="c-notification__new"></div>
-                    </div>
-
-                    <a
-                        onClick={
-                            (e) => {
-                                Variable.SetModals({ name: "ModalMobileMainSettings", data: {} })
-                                e.stopPropagation();
-                            }
-                        }
-                        class="c-userpanel__icon c-userpanel__icon--burger c-userpanel__icon--mobile_visible"
-                    ></a>
-                </div>
-                <div class="c-userpanel__addmodal">
-                    <div class="c-userpanel__inner">
-                        <p>{Variable.lang.button.create}</p>
-                        <a class="c-userpanel__link" onclick={Helpers.siteLink} href="/user/posts/">
-                            <img src={svg["profile_icon-5"]} />
-                            {Variable.lang.h.createPost}
-                        </a>
-                    </div>
-                    <div data-action="user_cabinet_add_close" class="c-userpanel__close">
-                        <img src={svg.close} />
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
-
-
     return (
         <div class="c-userpanel c-userpanel--bottom">
             <div class="c-userpanel__icons">
                 <a
-                    class="c-userpanel__icon c-userpanel__icon--logo c-userpanel__icon--mobile_visible"
                     href="/"
-                    onclick={Helpers.siteLink}>
-                    {/* <img src={svg["mini_logo"]} /> */}
+                    class="c-userpanel__icon c-userpanel__icon--logo c-userpanel__icon--mobile_visible"
+                    onclick={fn.siteLink}>
                 </a>
                 <a
-                    class={`c-userpanel__icon c-userpanel__icon--lenta c-userpanel__icon--mobile_visible ${Variable.dataUrl.adress == "lenta-users" ? "c-userpanel__icon--active" : ""}`}
                     href="/lenta-users/"
-                    onclick={Helpers.siteLink}
-                ></a>
-                <a
-                    class={`c-userpanel__icon c-userpanel__icon--blog c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "blog" && !Variable.dataUrl.category) ? "c-userpanel__icon--active" : ""}`}
-                    href="/blog/"
-                    onclick={Helpers.siteLink}
-                ></a>
+                    class={["c-userpanel__icon c-userpanel__icon--lenta c-userpanel__icon--mobile_visible", Variable.dataUrl.adress == "lenta-users" ? "c-userpanel__icon--active" : null]}
+                    onclick={fn.siteLink}>
 
+                </a>
+                {
+                    Variable.auth
+                        ?
+                        <a
+                            href="/user/chats/"
+                            class={["c-userpanel__icon c-userpanel__icon--chats c-userpanel__icon--disabled c-userpanel__icon--mobile_visible", (Variable.dataUrl.adress == "user" && Variable.dataUrl.category == "chats") ? "c-userpanel__icon--active" : null]}
+                            onclick={fn.siteLink}>
+                            {Variable.myInfo && Variable.myInfo.unreadMessage ? <div class="messages_notifications_counter">{Variable.myInfo.unreadMessage}</div> : null}
+                        </a>
+                        :
+                        <a
+                            href="/blog/"
+                            class={["c-userpanel__icon c-userpanel__icon--blog c-userpanel__icon--mobile_visible", (Variable.dataUrl.adress == "blog" && !Variable.dataUrl.category) ? "c-userpanel__icon--active" : null]}
+                            onclick={fn.siteLink}>
+
+                        </a>
+                }
                 <a
-                    onclick={(e) => {
-                        Variable.SetModals({ name: "ModalAuth", data: {} })
-                        e.stopPropagation();
-                    }}
+                    href="/user/posts/"
                     class="c-userpanel__icon c-userpanel__icon--posts c-userpanel__icon--mobile_visible"
-                ></a>
-                <a
-                    onclick={Helpers.siteLink}
-                    class={`c-userpanel__icon c-userpanel__icon--questions c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "question") ? "c-userpanel__icon--active" : ""}`}
-                    href="/question/"
-                ></a>
-                <a
-                    onclick={Helpers.siteLink}
-                    class={`c-userpanel__icon c-userpanel__icon--news c-userpanel__icon--mobile_visible ${(Variable.dataUrl.adress == "news") ? "c-userpanel__icon--active" : ""}`}
-                    href="/news/"
-                ></a>
-
-                <a
-                    onClick={
-                        (e) => {
-                            Variable.SetModals({ name: "ModalMobileMainSettings", data: {} })
-                            e.stopPropagation();
+                    onclick={(e) => {
+                        if (Variable.auth) {
+                            fn.siteLink(e)
+                        } else {
+                            fn.modals.ModalAuth()
                         }
-                    }
+                    }}>
+                </a>
+                <a
+                    href="/question/"
+                    class={["c-userpanel__icon c-userpanel__icon--questions c-userpanel__icon--mobile_visible", (Variable.dataUrl.adress == "question") ? "c-userpanel__icon--active" : null]}
+                    onclick={fn.siteLink}>
+                </a>
+                {
+                    Variable.auth
+                        ?
+                        <div class={["c-notification", (findUnread(Variable.notifyQuestions) || findUnread(Variable.notifyAwards) || findUnread(Variable.notifySystem)) ? "c-notification--active" : null]}>
+                            <a
+                                class="c-userpanel__icon c-userpanel__icon--notify c-userpanel__icon--mobile_visible c-notification__link"
+                                onClick={(e) => { e.stopPropagation(); fn.modals.ModalNotify(); }}>
+                            </a>
+                            <div class="c-notification__new"></div>
+                        </div>
+                        :
+                        <a
+                            href="/news/"
+                            class={["c-userpanel__icon c-userpanel__icon--news c-userpanel__icon--mobile_visible", (Variable.dataUrl.adress == "news") ? "c-userpanel__icon--active" : null]}
+                            onclick={fn.siteLink}>
+                        </a>
+                }
+                <a
                     class="c-userpanel__icon c-userpanel__icon--burger c-userpanel__icon--mobile_visible"
-                ></a>
+                    onClick={(e) => { e.stopPropagation(); fn.modals.ModalMobileMainSettings(); }}>
+                </a>
             </div>
-            <div class="c-userpanel__addmodal">
+            {/* <div class="c-userpanel__addmodal">
                 <div class="c-userpanel__inner">
                     <p>{Variable.lang.button.create}</p>
-                    <a class="c-userpanel__link" onclick={Helpers.siteLink} href="/user/posts/">
+                    <a class="c-userpanel__link" onclick={fn.siteLink} href="/user/posts/">
                         <img src={svg["profile_icon-5"]} />
                         {Variable.lang.h.createPost}
                     </a>
@@ -185,9 +95,9 @@ const BottomMenu = function () {
                 <div data-action="user_cabinet_add_close" class="c-userpanel__close">
                     <img src={svg.close} />
                 </div>
-            </div>
+            </div> */}
         </div>
-    );
+    )
 };
 export { BottomMenu };
 // OK
--- src/component/modals/ModalNotify.js
+++ src/component/modals/ModalNotify.js
@@ -6,6 +6,7 @@ import {
 	initOne,
 	sendApi,
 	Helpers,
+	init
 } from "@betarost/cemjs";
 import svg from "@assets/svg/index.js";
 import { NotifyItem } from '@component/element/NotifyItem.js';
@@ -14,7 +15,7 @@ let notify, currentNotify;
 
 
 
-const ModalNotify = async function (data, reload) {
+const ModalNotify = async function (data, ID) {
 
 	const changeCategory = async function () {
 		if (currentNotify[this.dataset.type]) {
@@ -50,67 +51,72 @@ const ModalNotify = async function (data, reload) {
 		initReload()
 	}
 
-	if (!reload) {
-		notify = Variable.myInfo.notifyQuestions;
+	init(
+		() => {
+			notify = Variable.myInfo.notifyQuestions;
 
-		currentNotify = {
-			question: true,
-			awards: false,
-			system: false
-		};
-	}
+			currentNotify = {
+				question: true,
+				awards: false,
+				system: false
+			};
+		},
+		() => {
+			return (
+				<div class="c-modal c-modal--open" id="ModalNotify">
+					<section class="c-modal__dialog">
+						{/* <header class="c-modal__header">
+							<h4>Уведомления</h4>
+							<button
+								type="button"
+								class="c-modal__close"
+								onclick={() => {
+									Variable.DelModals("ModalNotify")
+								}}
+							></button>
+						</header>
+						<div class="c-modal__body"> */}
+						<img
+							class="notify_close"
+							src={svg.close}
+							onclick={() => {
+								Variable.DelModals("ModalNotify")
+							}}
+						/>
+						<div class="notifications_title">
+							<div class="notifications_titles_line">
+								{Variable.lang.text.yourNotification}
+								<a data-action="link" href="/user/notify/">{Variable.lang.button.show_all}</a>
+							</div>
+							<div class="notifications_toggle_block">
+								<div data-type='question' onclick={changeCategory} class={currentNotify.question ? 'notifications_toggle_item notifications_toggle_item_active' : 'notifications_toggle_item'}>
+									{Variable.lang.text.questions}
+								</div>
+								<div data-type='awards' onclick={changeCategory} class={currentNotify.awards ? 'notifications_toggle_item notifications_toggle_item_active' : 'notifications_toggle_item'}>
+									{Variable.lang.text.awards}
+								</div>
+								<div data-type='system' onclick={changeCategory} class={currentNotify.system ? 'notifications_toggle_item notifications_toggle_item_active' : 'notifications_toggle_item'}>
+									{Variable.lang.text.system}
+								</div>
+							</div>
+						</div>
+						<div class="notifications_list">
+							<div class="notifications_list_inner">
+								<div class="notifications_list_part part_questions">
+									<NotifyItem
+										data={notify}
+									/>
+								</div>
+							</div>
+						</div>
+						{/* </div> */}
+					</section>
+				</div>
+			);
+		}, ID
+	)
 
-	return (
-		<div class="c-modal c-modal--open" id="ModalNotify">
-			<section class="c-modal__dialog">
-				{/* <header class="c-modal__header">
-					<h4>Уведомления</h4>
-					<button
-						type="button"
-						class="c-modal__close"
-						onclick={() => {
-							Variable.DelModals("ModalNotify")
-						}}
-					></button>
-				</header>
-				<div class="c-modal__body"> */}
-				<img
-					class="notify_close"
-					src={svg.close}
-					onclick={() => {
-						Variable.DelModals("ModalNotify")
-					}}
-				/>
-				<div class="notifications_title">
-					<div class="notifications_titles_line">
-						{Variable.lang.text.yourNotification}
-						<a data-action="link" href="/user/notify/">{Variable.lang.button.show_all}</a>
-					</div>
-					<div class="notifications_toggle_block">
-						<div data-type='question' onclick={changeCategory} class={currentNotify.question ? 'notifications_toggle_item notifications_toggle_item_active' : 'notifications_toggle_item'}>
-							{Variable.lang.text.questions}
-						</div>
-						<div data-type='awards' onclick={changeCategory} class={currentNotify.awards ? 'notifications_toggle_item notifications_toggle_item_active' : 'notifications_toggle_item'}>
-							{Variable.lang.text.awards}
-						</div>
-						<div data-type='system' onclick={changeCategory} class={currentNotify.system ? 'notifications_toggle_item notifications_toggle_item_active' : 'notifications_toggle_item'}>
-							{Variable.lang.text.system}
-						</div>
-					</div>
-				</div>
-				<div class="notifications_list">
-					<div class="notifications_list_inner">
-						<div class="notifications_list_part part_questions">
-							<NotifyItem
-								data={notify}
-							/>
-						</div>
-					</div>
-				</div>
-				{/* </div> */}
-			</section>
-		</div>
-	);
+
 };
 
 export default ModalNotify;
