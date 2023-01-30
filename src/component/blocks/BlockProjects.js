71ded9528cf9382687be2d85a7cdc0ee00821e08
Романова Виктория <tory_kuzya@mail.ru>
2023-01-30 15:24:04 +0300

    fix

--- src/component/blocks/BlockProjects.js
+++ src/component/blocks/BlockProjects.js
@@ -102,7 +102,7 @@ const BlockProjects = async function () {
                 {
                     "title": Variable.lang.a.icoRating,
                     "icon": "star1",
-                    "link": "ico-rating"
+                    "link": "list-icostartaps"//"ico-rating"
                 },
                 // {
                 //     "title": Variable.lang.a.trade,
