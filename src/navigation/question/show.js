add72c46f625644e94f9e4ac8f6080454f09f6cf
Романова Виктория <tory_kuzya@mail.ru>
2023-02-10 11:59:16 +0300

    Ответ на вопрос
    Форма ответа перенесена из модалки в форму на страницу

--- src/navigation/question/show.js
+++ src/navigation/question/show.js
@@ -5,17 +5,181 @@ import {
   init,
   initReload,
   load,
-  Data
+  Data,
+  sendApi,
+  Helpers,
 } from "@betarost/cemserver/cem.js";
 
 import { fn } from '@src/functions/index.js';
 import svg from '@assets/svg/index.js';
-import { Avatar, LentaMedia, Evaluation, ItemsMenu, ButtonShowMore, ButtonSubmit, TextArea, NotFound, Comment } from "@component/element/index.js";
+import { Avatar, LentaMedia, Evaluation, ItemsMenu, ButtonShowMore, ButtonSubmit, TextArea, NotFound, Comment, MediaButton, MediaPreview } from "@component/element/index.js";
 import { BlockError404 } from '@component/blocks/index.js';
 
+let elAnswerForm, elAnswerToggle, elText
 
 const start = function (data, ID) {
   let [Static, item] = fn.GetParams({ data, ID, initData: "question_show" })
+
+  const sendPhoto = async function (Static, crooper) {
+    if (!crooper) {
+      return
+    }
+    let canvas;
+    Static.mediaInputs.selectAspect = crooper.options.aspectRatio;
+    canvas = crooper.getCroppedCanvas({
+      // width: 166,
+      // height: 166,
+    });
+    let previewObj = {
+      src: canvas.toDataURL(),
+      type: "image",
+      upload: 0,
+      size: 0
+    };
+    Static.mediaInputs.show = true;
+    Static.mediaInputs.value.push(previewObj);
+    let numItem = Static.mediaInputs.value.length - 1
+    initReload();
+    await canvas.toBlob(function (blob) {
+      fn.uploadMedia(
+        blob,
+        "answers",
+        async function () {
+          Static.mediaInputs.show = true;
+          if (!this.response) {
+            return
+          }
+          let response = JSON.parse(this.response);
+          Static.mediaInputs.value[numItem] = {
+            aspect: Static.mediaInputs.selectAspect,
+            type: response.mimetype.split("/")[0],
+            name: response.name
+          };
+          Static.isValid = true;
+          initReload();
+        },
+        async function (e) {
+          let contentLength;
+          if (e.lengthComputable) {
+            contentLength = e.total;
+          } else {
+            contentLength = parseInt(
+              e.target.getResponseHeader(
+                "x-decompressed-content-length"
+              ),
+              10
+            );
+          }
+          if (Static.mediaInputs.value[numItem].upload === Static.mediaInputs.value[numItem].size && Static.mediaInputs.value[numItem].upload !== 0) {
+            Static.mediaInputs.value.splice(numItem, 1);
+            initReload()
+            return
+          }
+          Static.mediaInputs.value[numItem].upload = e.loaded
+          Static.mediaInputs.value[numItem].size = contentLength;
+          initReload();
+        }
+      );
+      initReload();
+    });
+    return
+  }
+
+  const sendVideo = async function (Static, files) {
+    fn.uploadMedia(
+      files[0],
+      "answers",
+      async function () {
+        Static.mediaInputs.show = true;
+        let tmp = JSON.parse(this.response);
+        let type = tmp.mimetype.split("/")[0];
+        let obj = { aspect: undefined, type, name: tmp.name };
+
+        Static.mediaInputs.value.push(obj);
+        initReload();
+      },
+      async function (e) {
+        let contentLength;
+        if (e.lengthComputable) {
+          contentLength = e.total;
+        } else {
+          contentLength = parseInt(
+            e.target.getResponseHeader(
+              "x-decompressed-content-length"
+            ),
+            10
+          );
+        }
+        // console.log(
+        //   "=3c5fa7= ",
+        //   "Загружено",
+        //   e.loaded,
+        //   "из",
+        //   contentLength
+        // );
+      }
+    );
+    return
+  }
+
+  const changeTextAnswer = (e) => {
+    // let text = wrapTextWithATag(e.target.innerText.trim());
+    let text = e.target.innerText.trim();
+    Static.textAnswer.error = "";
+
+    if (text.length === 0) {
+      Static.textAnswer.error = Variable.lang.error_div.not_empty_input;
+    } else if (text.length < 5) {
+      Static.textAnswer.error = Variable.lang.error_div.minSymbol;
+    } else if (text.length > 2000) {
+      Static.textAnswer.error = Variable.lang.error_div.maxSymbol;
+    }
+    Static.textAnswer.value = Helpers.editText(text, { clear: true })
+    // wrapTextWithATag(text);
+    if (Static.textAnswer.error === "") {
+      Static.isValid = true;
+    } else {
+      Static.isValid = false;
+    }
+    // initReload("modals");
+    initReload(ID);
+  };
+
+  const sendAnswer = async function (e, res) {
+    e.preventDefault();
+    if (!Static.isValid) {
+      return false;
+    }
+
+    let mediaArr = [];
+    let data = {
+      value: {
+        media: Static.mediaInputs.value,
+        questionId: res.item._id,
+        text: Static.textAnswer.value,
+      }
+    }
+    let tmpRes = await sendApi.create("setAnswer", data);
+    if (tmpRes.status === "ok") {
+      res.onClose()
+      // Variable.DelModals("ModalAnswer");
+      // Спрятать и очистить форму ответа
+
+      //initReload();
+    } else {
+      Variable.SetModals(
+        {
+          name: "ModalAlarm",
+          data: {
+            icon: "alarm_icon",
+            text: Variable.lang.error_div[tmpRes.error],
+          },
+        },
+        true
+      );
+    }
+    return;
+  };
 
   load({
     ID,
@@ -37,6 +201,17 @@ const start = function (data, ID) {
           rows: 7
         }
       }
+      Static.textAnswer = {
+        value: "",
+        error: "",
+        show: true,
+      }
+      Static.mediaInputs = {
+        value: [],
+        show: false,
+        selectAspect: null,
+      }
+      Static.isValid = false
 
     },
 
@@ -55,12 +230,9 @@ const start = function (data, ID) {
         elShowInput: {},
         el: {}
       }
-      Static.mediaInputs = {
-        value: [],
-        show: false,
-      }
 
       Static.elNumberSwiper = {}
+
 
       if (!item._id) { return (<div><BlockError404 /></div>) }
       console.log('=1b441a=', Static)
@@ -121,22 +293,34 @@ const start = function (data, ID) {
                     <div
                       class="btn-answer"
                       onclick={() => {
-                        fn.modals.ModalAnswer({
-                          item, onClose: async () => {
+                        if (elAnswerForm.hidden) {
+                          elAnswerForm.hidden = false;
+                          elAnswerToggle.innerHTML = '<span>' + Variable.lang.button.resetCrop + '</span>'
+                        } else {
+                          elAnswerForm.hidden = true;
+                          elAnswerToggle.innerHTML = '<span>' + Variable.lang.button.giveAnswer + '</span>'
+                        }
+                        // fn.modals.ModalAnswer({
+                        //   item, onClose: async () => {
 
-                            setTimeout(async function () {
+                        //     setTimeout(async function () {
 
-                              Static.itemAnswer = await fn.restApi.getAnswers({ filter: { questionId: item._id }, limit: 6, sort: { showDate: -1 } })
+                        //       Static.itemAnswer = await fn.restApi.getAnswers({ filter: { questionId: item._id }, limit: 6, sort: { showDate: -1 } })
 
-                              initReload()
-                            }, 700)
-                            return (
-                              <img src={svg['load']} />
-                            )
-                          }
-                        })
+                        //       initReload()
+                        //     }, 700)
+                        //     return (
+                        //       <img src={svg['load']} />
+                        //     )
+                        //   }
+                        // })
                       }}>
-                      <a class="btn-gr-answer">
+                      <a
+                        class="btn-gr-answer"
+                        Element={($el) => {
+                          elAnswerToggle = $el
+                        }}
+                      >
                         <span>{Variable.lang.button.giveAnswer}</span>
                       </a>
                     </div>
@@ -144,6 +328,177 @@ const start = function (data, ID) {
                     null
                 }
               </div>
+            </div>
+            <div
+              class="c-answers__answer"
+              hidden={true}
+              Element={($el) => {
+                elAnswerForm = $el
+              }}
+            >
+              <form id="createPostForm" onsubmit={(e) => sendAnswer(e, { item: item })} >
+                <input style="display: none;" type="submit" />
+                <div class="error-div">
+                  <div class="error-div-variant">{Static.textAnswer.error}</div>
+                </div>
+                <div data-type="answers" class="create_post_container">
+                  <div
+                    class="c-chapter create_post_chapter create_post_main_text"
+                    contenteditable="true"
+                    oninput={changeTextAnswer}
+                    Element={($el) => {
+                      elText = $el
+                    }}
+                  ></div>
+                  {() => {
+                    if (Static.mediaInputs.show && Static.mediaInputs.value.length) {
+                      return (
+                        <div class="create_post_chapter createPostImage">
+                          {
+                            Static.mediaInputs.value.map((item, index) => {
+                              if (item.type != "audio") {
+                                return (
+                                  <MediaPreview
+                                    item={item}
+                                    index={index}
+                                    type="answers"
+                                    // formInputs={formInputs}
+                                    Static={Static}
+                                  />
+                                );
+                              }
+                            })
+                          }
+                        </div>
+                      )
+                    }
+                  }}
+
+                  {() => {
+                    if (Static.mediaInputs.show && Static.mediaInputs.value.length && Static.mediaInputs.value.filter((item) => item.type == "audio").length) {
+                      return (
+                        <div class="create_post_chapter createPostAudio">
+                          {/* <Map
+                          data={formInputs.mediaInputs.value}
+                          dataIf={(item, index) => {
+                            if (item.type == "audio") {
+                              return (
+                                <MediaPreview
+                                  item={item}
+                                  index={index}
+                                  type="answers"
+                                  formInputs={formInputs}
+                                />
+                              );
+                            }
+                          }}
+                        /> */}
+                          {
+                            Static.mediaInputs.value.map((item, index) => {
+                              if (item.type == "audio") {
+                                return (
+                                  <MediaPreview
+                                    item={item}
+                                    index={index}
+                                    type="answers"
+                                    // formInputs={formInputs}
+                                    Static={Static}
+                                  />
+                                );
+                              }
+                            })
+                          }
+                        </div>
+                      )
+                    }
+                  }}
+                </div>
+
+                <MediaButton
+
+                  // onclickText={function () {
+                  //   if (formInputs.textAnswer.show === true) {
+                  //     return;
+                  //   } else {
+                  //     formInputs.textAnswer.show = true;
+                  //     initReload("modals");
+                  //   }
+                  // }}
+
+                  onclickPhoto={function () {
+                    if (this.files.length == 0) {
+                      return;
+                    }
+
+                    Variable.SetModals({
+                      name: "ModalCropImage",
+                      data: {
+                        file: this.files[0],
+                        typeUpload: 'answers',
+                        arrMedia: Static.mediaInputs.value,
+                        aspectSelect: Static.mediaInputs.selectAspect,
+                        uploadCropImage: async function (cropper) {
+                          await sendPhoto(Static, cropper)
+                          return;
+                        }
+                      },
+                    }, true);
+                    // formInputs.isValid = true;
+                    this.value = '';
+                  }}
+
+                  onclickVideo={function () {
+                    if (this.files.length == 0) {
+                      return;
+                    }
+                    sendVideo(Static, this.files)
+                    this.value = '';
+                    return;
+                  }}
+                />
+                <button
+                  class={[
+                    "c-button c-button--gradient2",
+                    !Static.isValid ? "c-button--inactive" : "",
+                  ]}
+                  type="button"
+                  // ref={elemButton}
+                  onClick={(e) => {
+                    console.log('=719e4a=', item); sendAnswer(e, {
+                      item, onClose: async () => {
+                        Static.textAnswer = {
+                          value: "",
+                          error: "",
+                          show: true,
+                        }
+                        Static.mediaInputs = {
+                          value: [],
+                          show: false,
+                          selectAspect: null,
+                        }
+                        Static.isValid = false
+
+                        elText.innerText = '';
+                        
+                        setTimeout(async function () {
+
+                          Static.itemAnswer = await fn.restApi.getAnswers({ filter: { questionId: item._id }, limit: 6, sort: { showDate: -1 } })
+                          elAnswerForm.hidden = true;
+                          elAnswerToggle.innerHTML = '<span>' + Variable.lang.button.giveAnswer + '</span>'
+
+
+                          initReload()
+                        }, 700)
+                        return (
+                          <img src={svg['load']} />
+                        )
+                      }
+                    })
+                  }}
+                >
+                  <span class="c-button__text">{Variable.lang.button.send}</span>
+                </button>
+              </form>
             </div>
             <div class="user_news_block">
               {
