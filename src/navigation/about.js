556d4466725edcccf285e618b5a7e8d8350d0447
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-02-22 08:05:09 +0300

    about us
    about us

--- src/navigation/about.js
+++ src/navigation/about.js
@@ -20,32 +20,22 @@ const team = [
   {
     name: "Ян Кривоносов",
     foto: "startaps-inner/team1",
-    position: "Основатель проекта «Сrypto Emergency»",
+    position: "CEO проекта Crypto Emergency",
   },
   {
     name: "Игорь Еньшин",
     foto: "startaps-inner/team4",
-    position: "Web-программист",
+    position: "Руководитель IT направления",
   },
   {
     name: "Анна Рыжкова",
     foto: "startaps-inner/team2",
-    position: "Основатель проекта «Сrypto Emergency»",
+    position: "Руководитель отдела по работе с дизайнерами",
   },
   {
     name: "Дмитрий Белов",
     foto: "startaps-inner/team3",
-    position: "Основатель проекта «Сrypto Emergency»",
-  },
-  {
-    name: "Анна Шалбузова",
-    foto: "startaps-inner/team7",
-    position: "Основатель проекта «Сrypto Emergency»",
-  },
-  {
-    name: "Дмитрий Белов",
-    foto: "startaps-inner/team3",
-    position: "Основатель проекта «Сrypto Emergency»",
+    position: "Управляющий директор проекта",
   },
 ];
 
@@ -59,15 +49,24 @@ const arrAccordeon = [
   {
     title: "Как заработать или купить токены CEM?",
     description:
-      "Купить CEM на Бирже Bitmart. Вести активную деятельность на платформе и получать новый уровень за действия на платформе и с новым уровнем получать в награду CEM. Следить за новостями в телеграме, могут появляться разные конкурсы где в награду можно получить CEM",
+      "Купить CEM можно официально на бирже Bitmart. Также можно вести активную деятельность на платформе и получать новый уровень за действия на платформе и с новым уровнем получать в награду CEM. Следите за новостями в телеграме, могут появляться разные конкурсы где в награду можно получить монету CEM.",
     hidden: true,
   },
   {
     title: "Как вывести заработанные CEM с платформы?",
     description:
-      "Вывести заработанные CEM на нашей платформе за проявленную активность Вы можете на свой счет в любое время. На данный момент CEM торгуется на бирже  Bitmart, на официальном сайте бирже, переходите в раздел торговать и в поиске пишите CEM. ",
-
+      "Вывести заработанные CEM на нашей платформе за проявленную активность вы можете на свой счет в любое время. На данный момент вывод средств осуществляется путем подачи заявки через администратора.",
     hidden: true,
+  },
+  {
+    title: "Проект доступен лишь в России?",
+    description:  "У нас огромная экосистема которая на данный момент полностью переведена на 16 языков по всему миру, а основными разделами платформы уже сейчас можно пользоваться на 60 языках. Платформа доступна на всех рынках включая Китай.",
+    hidden: true,
+  },
+  {
+    title: "Можно ли скачать приложение на телефон?",
+    description: "Да! Все проекты нашей экосистемы доступны как на Android так и на iPhone. Найти ссылочки для скачивания можно внизу страницы, либо просто через поиск в маркетах.",
+    hidden: false,
   },
 ];
 
@@ -201,78 +200,28 @@ const start = function (data, ID) {
                       title={item.title}
                       description={item.description}
                       hidden={item.hidden}
-                      // Element={(el) => {
-                      //   item.element = el;
-                      // }}
                       onclick={() => {
-                        // console.log("hidden before", item.hidden);
-                        arrAccordeon.forEach((u, index) => {
-                          // u.element.hidden = true;
+                        arrAccordeon.forEach((u) => {
                           u.hidden = true;
-                          // console.log("item element", u.element.hiden);
                         });
-
-                        // item.element.hidden = false;
-
                         item.hidden = false;
+                        // if(!item.hidden){
+                        //   item.hidden = !item.hidden
+                        // }
                         initReload();
-                        // this.nextElementSibling.classList.add("content-hidden");
-                        // console.log("hidden after", item.hidden);
                       }}
                     />
                   );
                 })}
               </div>
 
-              <div class="roadmap">
-                <h2>{Variable.lang.h.road_map}</h2>
-                <div class="roadmap-wrap">
-                  <div class="roadmap_item roadmap_item--0">
-                    <span class="year year--0">
-                      {Variable.lang.p.mapDateOne}
-                    </span>
-                    <p class="desc">{Variable.lang.span.mapDescOne}</p>
-                  </div>
-                  <div class="roadmap_item roadmap_item--1">
-                    <span class="year year--1">
-                      {Variable.lang.p.mapDateThree}
-                    </span>
-                    <p class="desc">{Variable.lang.span.mapDescFive}</p>
-                  </div>
-                  <div class="roadmap_item roadmap_item--2">
-                    <span class="year year--2">
-                      {Variable.lang.p.October2022}
-                    </span>
-                    <p class="desc">{Variable.lang.span.mapDescFour}</p>
-                  </div>
-                  <div class="roadmap_item roadmap_item--3">
-                    <span class="year year--3">
-                      {Variable.lang.p.mapDateTwo}
-                    </span>
-                    <p class="desc">{Variable.lang.span.mapDescTwo}</p>
-                  </div>
-                  <div class="roadmap_item roadmap_item--4">
-                    <span class="year year--4">
-                      {Variable.lang.p.mapDateFour}
-                    </span>
-                    <p class="desc">{Variable.lang.span.mapDescThree}</p>
-                  </div>
-                  <div class="roadmap_item roadmap_item--5">
-                    <span class="year year--5">
-                      {Variable.lang.p.mapDateSix}
-                    </span>
-                    <p class="desc">{Variable.lang.span.mapDescSix}</p>
-                  </div>
-                </div>
-              </div>
-              {/*
               <div class="team">
                 <h2 class="startap-title">{Variable.lang.h.our_team}</h2>
                 <div class="swiper mySwiper" After={() => swiperGo()}>
                   <div class="swiper-wrapper">{showTeam(team)}</div>
                   <div class="swiper-pagination"></div>
                 </div>
-              </div> */}
+              </div>
 
               {/* <div class="c-goals__list">
                 <div class="c-goals__item">
@@ -329,7 +278,85 @@ const start = function (data, ID) {
 
             <div class="c-aboutus__roadmap">
               <h2>{Variable.lang.h.road_map}</h2>
-              <div class="c-aboutus__wrapper">
+              <div class="roadmap-wrap">
+                <div class="roadmap_item roadmap_item--0">
+                  <span class="year year--0">
+                    {Variable.lang.span.mapDescOne}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateOne}</p>
+                </div>
+
+                <div class="turn turn-0">
+                  <img alt={Variable.lang.span.mapDescOne} src={svg['roadmap/turn-left1']}></img>
+                </div>
+
+                <div class="roadmap_item roadmap_item--1">
+                  <span class="year year--1">
+                    {Variable.lang.span.mapDescTwo}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateTwo}</p>
+                </div>
+
+                <div class="turn turn-1">
+                  <img alt={Variable.lang.span.mapDescTwo} src={svg['roadmap/turn-right2']}></img>
+                </div>
+
+                <div class="roadmap_item roadmap_item--2">
+                  <span class="year year--2">
+                    {Variable.lang.span.mapDescThree}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateThree}</p>
+                </div>
+
+                <div class="turn turn-2">
+                  <img alt={Variable.lang.span.mapDescThree} src={svg['roadmap/turn-left3']}></img>
+                </div>
+
+                <div class="roadmap_item roadmap_item--3">
+                  <span class="year year--3">
+                    {Variable.lang.span.mapDescFour}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateFour}</p>
+                </div>
+
+                <div class="turn turn-3">
+                  <img alt={Variable.lang.span.mapDescFour} src={svg['roadmap/turn-right4']}></img>
+                </div>
+
+                <div class="roadmap_item roadmap_item--4">
+                  <span class="year year--4">
+                    {Variable.lang.span.mapDescFive}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateFive}</p>
+                </div>
+
+                <div class="turn turn-4">
+                  <img alt={Variable.lang.span.mapDescFive} src={svg['roadmap/turn-left5']}></img>
+                </div>
+
+                <div class="roadmap_item roadmap_item--5">
+                  <span class="year year--5">
+                    {Variable.lang.span.mapDescSix}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateSix}</p>
+                </div>
+
+                <div class="turn turn-5">
+                  <img alt={Variable.lang.span.mapDescSix} src={svg['roadmap/turn-right6']}></img>
+                </div>
+
+                <div class="roadmap_item roadmap_item--6">
+                  <span class="year year--6">
+                    {Variable.lang.span.mapDescSeven}
+                  </span>
+                  <p class="desc">{Variable.lang.p.mapDateSeven}</p>
+                </div>
+
+                <div class="turn turn-6">
+                  <img alt={Variable.lang.span.mapDescSeven} src={svg['roadmap/turn-left7']}></img>
+                </div>
+              </div>
+              {/* <div class="c-aboutus__wrapper">
                 <div class="c-aboutus__toppart">
                   <div class="c-aboutus__dateitem c-aboutus__dateitem--1">
                     <p>{Variable.lang.p.mapDateOne}</p>
@@ -359,7 +386,7 @@ const start = function (data, ID) {
                     <span>{Variable.lang.span.mapDescSix}</span>
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
 
             <div class="c-aboutus__projects">
