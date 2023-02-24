b31ef9e1d4b703ef58967239a867f834a599d2d0
Betarost <betarost@yandex.ru>
2023-02-24 15:16:05 +0300

    .

--- src/navigation/media/show.js
+++ src/navigation/media/show.js
@@ -2,10 +2,11 @@ import {
   jsx,
   jsxFrag,
   load,
-  Helpers
+  Variable
 } from "@betarost/cemserver/cem.js";
 
 import { fn } from '@src/functions/export.js';
+import svg from "@assets/svg/index.js";
 import Elements from "@src/elements/export.js";
 
 import { BlockShowNews, BlockError404 } from '@component/blocks/index.js';
@@ -26,17 +27,21 @@ const start = function (data, ID = "mainBlock") {
       return (
         <Elements.page.MainContainer title={Static.item.title}>
           <Elements.page.Container class="p-lr">
-            {/* <div class="full_news_container"> */}
-            {/* <div class="full_news_block"> */}
-            {/* <div class="full_news_content"> */}
             {Static.item.image ? <img class="full_news_image" src={"/assets/upload/news/" + Static.item.image} /> : null}
             {Static.item.preview ? <p class="full_news_text mrb30">{Static.item.preview}</p> : null}
-            {/* <p class="full_news_text mr20" tohtml={true}>{Helpers.stringToHtml(Static.item.text)}</p> */}
             <p class="full_news_text mr20" tohtml={true}>{Static.item.text}</p>
+            {Static.item.source ? <p class="full_news_disclaimer mr20">{Variable.lang.p.source}: <noindex><a href={Static.item.source} rel="nofollow noopener" target="_blank">{fn.Str.domain(Static.item.source)}</a></noindex></p> : null}
+            <div style="display: flex" class="blog_post_stat">
+              <p class="full_news_date">
+                <img src={svg["question_views"]} /> {Static.item.statistic.view}
+              </p>
+              <p class="full_news_date">
+                <img src={svg["question_answers"]} />
+                {Static.item.statistic.comments}
+              </p>
+              {/* <p class="full_news_date">{fn.getDateFormat(Static.item.showDate)}</p> */}
+            </div>
             {/* <BlockShowNews Static={Static} item={item} /> */}
-            {/* </div> */}
-            {/* </div> */}
-            {/* </div> */}
           </Elements.page.Container>
         </Elements.page.MainContainer>
       );
