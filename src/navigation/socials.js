90af290c69b81fa8b04fdd33838ac53c0036fd6b
Романова Виктория <Danil25082004@gmail.com>
2023-03-29 17:02:39 +0300

    SocialCia

--- src/navigation/socials.js
+++ src/navigation/socials.js
@@ -19,22 +19,60 @@ const start = function (data, ID) {
     fn: () => {
       return (
         <div class='c-main__body'>
-          <div class="titul_page">
+          <div class="titul_page style_titul">
             <div> Наши соц.сети </div>
 
-            <a href="https://t.me/emergencycrypto" target="_blank" class="Button_1">
+            <div class=" group_batton_1">
+              <a href="https://t.me/emergencycrypto" target="_blank" class="Button_1 style_titul">
+                Telegram
+              </a>
+              <a class=" Batton_1-2">
+                Telegram
+              </a>
+            </div>
 
-              Моя супер желтая кнопк
-            </a>
-            <a href="https://www.tiktok.com/@cryptoemergencyrussia" target="_blank" class="Button_2">
-              Тики токи :
-            </a>
-            <a href="https://www.youtube.com/channel/UCb9Fx-fNikzs-OZwnTXepLg" target="_blank" class="Button_3">
-              Ютубчик *ХЪ
 
-            </a>
+            <div class=" group_batton_2">
+              <a href="https://www.tiktok.com/@cryptoemergencyrussia" target="_blank" class="Button_2-1">
+                Тik tok
+              </a>
+              <a href="https://www.tiktok.com/@cryptoemergencyrussia" target="_blank" class="Button_2-2">
+                Тik tok
+              </a>
+            </div>
+
+
+            <div class=" group_batton_3">
+              <a href="https://www.youtube.com/channel/UCb9Fx-fNikzs-OZwnTXepLg" target="_blank" class="Button_3">
+                Youtube
+
+              </a>
+              <a href="https://www.youtube.com/channel/UCb9Fx-fNikzs-OZwnTXepLg" target="_blank" class="Button_3-2">
+                Youtube
+
+              </a>
+            </div>
+            <div>
+
+
+
+            </div>
             {/* <img src={svg["testTelega"]} > */}
           </div>
+
+
+
+          <div class="Our_sait">
+            <div>
+              Наши сайты
+              <a href="#" class="Button_sait">
+
+
+              </a>
+            </div>
+          </div>
+
+
         </div>
       );
     },
