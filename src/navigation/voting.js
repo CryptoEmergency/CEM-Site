92070055da3ec15d30b06679833319ac05103263
ArdikRj <ilya.solyannikov@bk.ru>
2023-03-29 17:12:41 +0300

    comet

--- src/navigation/voting.js
+++ src/navigation/voting.js
@@ -7,7 +7,24 @@ const start = function (data, ID) {
   load({
     ID,
     fn: () => {
-      return <div class="voting">Илья</div>;
+      return (
+        <div class="Main_vers">
+        <div class="voting">
+          <p class = "text">Голосование</p>
+          <div class="Opis">
+            Описание
+          </div>
+          <ol class="list">
+            <li class="list-city">Москва</li>
+            <li>Новороссийск</li>
+            <li>Краснодар</li>
+          </ol>
+        </div>
+        <div>
+<a class="voice"> Проголосовать</a>
+        </div>
+        </div>
+      )
     },
   });
 };
