d06232271bba215ce011080871202d547bfc1275
ArdikRj <ilya.solyannikov@bk.ru>
2023-03-31 17:05:44 +0300

    page voice

--- src/navigation/voting.js
+++ src/navigation/voting.js
@@ -1,4 +1,4 @@
-import { jsx, jsxFrag, load } from "@betarost/cemserver/cem.js";
+import { jsx, jsxFrag, load,  } from "@betarost/cemserver/cem.js";
 
 import { fn } from "@src/functions/index.js";
 
@@ -8,23 +8,31 @@ const start = function (data, ID) {
     ID,
     fn: () => {
       return (
-        <div class="Main_vers">
-        <div class="voting">
-          <p class = "text">Голосование</p>
-          <div class="Opis">
-            Описание
-          </div>
-          <ol class="list">
-            <li class="list-city">Москва</li>
-            <li>Новороссийск</li>
-            <li>Краснодар</li>
-          </ol>
+        <div class="container-voice">
+          <h1 class="voice-title">Голосование</h1>   
+            <div class="voice-inner">
+              <h3 class="name">Название</h3>
+              <p class="description">Описание описание описание описание описание</p>
+              <ol class="list-voice">
+                <li class="list-el">
+                  1. Москва
+                  <input type="checkbox" class="checkbox-train"></input>
+                </li>
+                <li class="list-el">  
+                  2. Новороссийск
+                <input type="checkbox" class="checkbox-train"></input>
+                </li>
+                <li class="list-el"> 
+                  3. Краснодар 
+                <input type="checkbox" class="checkbox-train"></input>
+                </li>
+              </ol>
+              <div class="wrap-btn">
+                <button class={["btn", Static.checked ? "btn-checked" : null]} > Проголосовать </button>
+              </div>
+            </div>     
         </div>
-        <div>
-<a class="voice"> Проголосовать</a>
-        </div>
-        </div>
-      )
+      );
     },
   });
 };
