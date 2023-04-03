59c42fcd62cd9c7e56eeea3d53cd9e140a160aa6
ArdikRj <ilya.solyannikov@bk.ru>
2023-04-03 15:19:44 +0300

    voiting

--- src/navigation/voting.js
+++ src/navigation/voting.js
@@ -1,4 +1,4 @@
-import { jsx, jsxFrag, load,  } from "@betarost/cemserver/cem.js";
+import { jsx, jsxFrag, load } from "@betarost/cemserver/cem.js";
 
 import { fn } from "@src/functions/index.js";
 
@@ -9,28 +9,157 @@ const start = function (data, ID) {
     fn: () => {
       return (
         <div class="container-voice">
-          <h1 class="voice-title">Голосование</h1>   
+          <h1 class="voice-title">Голосование</h1>
+          <div class="btns-wrap"> 
+          <div class="voice-inner">
+            <h3 class="name">Название</h3>
+            <p class="description">
+              Описание описание описание описание описание
+            </p>
+            <ol class="list-voice">
+              <li class="list-el">
+                <span>1. Москва</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>2. Новороссийск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>3. Краснодар</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>4. Санкт-Петербург</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>5. Екатеринбург</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>6. Казань</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>7. Нижний Новгород</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li><li class="list-el">
+                <span>8. Текст текст</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>9. Челябинск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+            </ol>
+            <div class="wrap-btn">
+              <button class={["btn", Static.checked ? "btn-checked" : null]}>
+                {" "}
+                Проголосовать{" "}
+              </button>
+            </div>
+          </div>
+
+          <div class="voice-inner">
+            <h3 class="name">Название</h3>
+            <p class="description">
+              Описание описание описание описание описание
+            </p>
+            <ol class="list-voice">
+              <li class="list-el">
+                <span>1. Хабаровск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>2. Ульяновск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>3. Иркутск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>4. Красноярск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>5. Самара</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>6. Уфа</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>7. Ростов-на-Дону</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li><li class="list-el">
+                <span>8. Текст текст</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>9. Омск</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+            </ol>
+            <div class="wrap-btn">
+              <button class={["btn", Static.checked ? "btn-checked" : null]}>
+                {" "}
+                Проголосовать{" "}
+              </button>
+            </div>
+            </div>
             <div class="voice-inner">
-              <h3 class="name">Название</h3>
-              <p class="description">Описание описание описание описание описание</p>
-              <ol class="list-voice">
-                <li class="list-el">
-                  1. Москва
-                  <input type="checkbox" class="checkbox-train"></input>
-                </li>
-                <li class="list-el">  
-                  2. Новороссийск
+            <h3 class="name">Название</h3>
+            <p class="description">
+              Описание описание описание описание описание
+            </p>
+            <ol class="list-voice">
+              <li class="list-el">
+                <span>1. Барнаул</span>
                 <input type="checkbox" class="checkbox-train"></input>
-                </li>
-                <li class="list-el"> 
-                  3. Краснодар 
+              </li>
+              <li class="list-el">
+                <span>2. Ижевск</span>
                 <input type="checkbox" class="checkbox-train"></input>
-                </li>
-              </ol>
-              <div class="wrap-btn">
-                <button class={["btn", Static.checked ? "btn-checked" : null]} > Проголосовать </button>
-              </div>
-            </div>     
+              </li>
+              <li class="list-el">
+                <span>3. Махачкала</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>4. Воронеж</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>5. Пермь</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>6. Волгоград</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>7. Саратов</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li><li class="list-el">
+                <span>8. Тюмень</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+              <li class="list-el">
+                <span>9. Тольятти</span>
+                <input type="checkbox" class="checkbox-train"></input>
+              </li>
+            </ol>
+            <div class="wrap-btn">
+              <button class={["btn", Static.checked ? "btn-checked" : null]}>
+                {" "}
+                Проголосовать{" "}
+              </button>
+            </div>
+            </div>
+          </div>
         </div>
       );
     },
