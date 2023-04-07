8f3ecf693931e4b5547ac79b3fb3cdc39323daaf
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-04-07 13:25:38 +0300

    .

--- src/component/modals/ModalFilterCoin.js
+++ src/component/modals/ModalFilterCoin.js
@@ -4,16 +4,16 @@ import {
   Variable,
   initReload,
   load,
-  init
+  init,
 } from "@betarost/cemserver/cem.js";
 
-import { fn } from '@src/functions/index.js';
+import { fn } from "@src/functions/index.js";
 
 const ModalFilterCoin = function (data, ID) {
-  let [Static] = fn.GetParams({ data, ID })
+  let [Static] = fn.GetParams({ data, ID });
 
-  Static.closeOut = true
-  Static.mouseOut = false
+  Static.closeOut = true;
+  Static.mouseOut = false;
   Static.allCoin = Static.list_coins;
   // Static.filterCoins = []
 
@@ -25,108 +25,118 @@ const ModalFilterCoin = function (data, ID) {
           class="c-modal c-modal--open"
           onclick={function (e) {
             if (Static.mouseOut && Static.closeOut) {
-              fn.modals.close(ID)
+              fn.modals.close(ID);
             }
-          }}>
+          }}
+        >
           <section
             class="c-modal__dialog"
             onmouseover={function () {
-              Static.mouseOut = false
+              Static.mouseOut = false;
             }}
             onmouseleave={function () {
-              Static.mouseOut = true
-            }}>
-
+              Static.mouseOut = true;
+            }}
+          >
             <header class="c-modal__header">
               <h2 class="c-modal__title">{Variable.lang.h.modal_listCoin}</h2>
               <button
                 class="c-modal__close"
                 onclick={() => {
-                  fn.modals.close(ID)
+                  fn.modals.close(ID);
                 }}
               ></button>
             </header>
 
             <div class="c-modal__body">
-
               <div class="coinForm-wrap">
-                <form
-                  class="filter-coinForm"
-                >
+                <form class="filter-coinForm">
                   <input
                     Element={($el) => {
-                      Static.elInput = $el
+                      Static.elInput = $el;
                     }}
                     type="text"
                     class="filter-coinInput"
                     placeholder={Variable.lang.h.modal_listCoin}
                     oninput={function () {
-                      let searchText = this.value.toLowerCase()
+                      let searchText = this.value.toLowerCase();
                       Static.allCoin = Static.list_coins.filter((item) => {
                         if (item.name.toLowerCase().includes(searchText)) {
-                          return true
+                          return true;
                         }
-                      })
-                      Static.elBtn.classList.toggle('content-hidden', !Static.elInput.value)
-                      initReload("modals")
+                      });
+                      Static.elBtn.classList.toggle(
+                        "content-hidden",
+                        !Static.elInput.value
+                      );
+                      initReload("modals");
                     }}
                   />
                   <span
                     Element={($el) => {
-                      Static.elBtn = $el
+                      Static.elBtn = $el;
                     }}
                     class={["close-icon", "content-hidden"]}
                     onclick={() => {
                       // Static.elForm.reset()
-                      Static.allCoin = Static.list_coins
-                      Static.elInput.value = ""
-                      initReload("modals")
-
-
+                      Static.allCoin = Static.list_coins;
+                      Static.elInput.value = "";
+                      initReload("modals");
                     }}
-                  >X</span>
+                  >
+                    X
+                  </span>
                 </form>
               </div>
 
               <div class="filterCoinContainer">
                 {Static.allCoin.map((item) => {
                   return (
-                    <div class={["coin-item", Static.filterCoins.includes(item.name) ? "coin-item_active" : null]}
+                    <div
+                      class={[
+                        "coin-item",
+                        Static.filterCoins.includes(item.name)
+                          ? "coin-item_active"
+                          : null,
+                      ]}
                       onclick={() => {
                         if (Static.filterCoins.includes(item.name)) {
-                          Static.filterCoins.splice(Static.filterCoins.indexOf(item.name), 1)
+                          Static.filterCoins.splice(
+                            Static.filterCoins.indexOf(item.name),
+                            1
+                          );
                         } else {
-                          Static.filterCoins.push(item.name)
+                          Static.filterCoins.push(item.name);
                         }
-                        initReload("modals")
-                      }}>
+                        initReload("modals");
+                      }}
+                    >
                       <div class="coin-item_img">
                         <img src={`/assets/icons/coins/${item.icon}.svg`}></img>
                       </div>
                       <span class="coin-item_text">{item.name}</span>
                     </div>
-                  )
+                  );
                 })}
               </div>
             </div>
             <div class="c-modal__footer">
               <button
-                class={["c-button", "c-button--gradient2",]}
+                class={["c-button", "c-button--gradient2"]}
                 type="button"
                 onclick={() => {
-                  Static.callback(Static.filterCoins)
-                  fn.modals.close(ID)
-                }}>
-                <span class="c-button__text">
-                  {Variable.lang.button.apply}
-                </span>
+                  Static.callback(Static.filterCoins);
+                  fn.modals.close(ID);
+                }}
+              >
+                <span class="c-button__text">{Variable.lang.button.apply}</span>
               </button>
             </div>
           </section>
         </div>
       );
-    }
-  })
-}
+    },
+  });
+};
 
 export default ModalFilterCoin;
