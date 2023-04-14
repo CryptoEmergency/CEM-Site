042dd9c7f5251b82c71cf1fdcb248bb8085cbcce
Романова Виктория <Danil25082004@gmail.com>
2023-04-14 17:38:00 +0300

    modal

--- src/component/modals/ModalAskQuestion.js
+++ src/component/modals/ModalAskQuestion.js
@@ -314,16 +314,16 @@ const ModalAskQuestion = function (data, ID) {
                         return (
                           <div
                             contenteditable="true"
-                            onpaste={function(e){
+                            onpaste={function (e) {
                               alert(1)
                               e.preventDefault()
-                             
+
                             }}
                             oninput={function (e) {
-                              if(e.inputType == "insertFromPaste"){
+                              if (e.inputType == "insertFromPaste") {
                                 console.log(e)
                               }
-                              
+
                               Static.close = false
                               Static.textQuestion.value = this.innerText;
 
