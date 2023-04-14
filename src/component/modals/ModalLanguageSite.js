3b79fd2394357fd373fc30758aff182f70c38b29
maksdrop <maksondmb2018@gmail.com>
2023-04-14 11:01:16 +0300

    .

--- src/component/modals/ModalLanguageSite.js
+++ src/component/modals/ModalLanguageSite.js
@@ -4,15 +4,14 @@ import {
   Variable,
   initReload,
   parsingUrl,
-  init,
   load
 } from "@betarost/cemserver/cem.js";
-import { fn } from "@src/functions/index.js";
+import { fn } from "@src/functions/export.js";
 
 
 
 const ModalLanguageSite = function (data, ID) {
-  let close
+  // let close
   load({
     ID,
     fnLoad: async () => {
@@ -22,26 +21,21 @@ const ModalLanguageSite = function (data, ID) {
       return (
         <div class="c-modal c-modal--open" onclick={function (e) {
           if (close) {
-
             fn.modals.close(ID)
           }
         }}>
           <section class="c-modal__dialog" onmouseover={function () {
-
             close = false
-
           }}
             onmouseleave={function () {
-
               close = true
-
             }}>
             <header class="c-modal__header c-modal__header--nopadding">
               <h4 class="c-changelanguage__title">{Variable.lang.h.modal_listLanguage}</h4>
               <button
                 class="c-modal__close"
                 onclick={() => {
-                  Variable.DelModals("ModalLanguageSite");
+                  fn.modals.close(ID)
                   // initReload("modals");
                 }}
               ></button>
