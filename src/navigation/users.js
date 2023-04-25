6fffe6211924f998895ec27ae1227618722bd074
Betarost <betarost@yandex.ru>
2023-04-25 17:32:17 +0300

    .

--- src/navigation/users.js
+++ src/navigation/users.js
@@ -34,6 +34,7 @@ const start = function (data, ID) {
                                 null
                         }
                         <div class="page-main__content">
+
                             <div class="filters">
                                 <div class="filters__container">
                                     <div class="filters__search filters__search_icon_loupe">
@@ -60,16 +61,65 @@ const start = function (data, ID) {
                                     <div class="filters__advanced">
                                         <div class="filters__select">
                                             {Variable.lang.text.language}
-                                            <img style="display: none;" class="refresh_language" src={svg.refresh_filter} />
                                         </div>
-
-                                        <div class="c-friends__wrapper">
-                                            <Input classDiv="language_select_wrapper" className="c-friends__lang" Static={Static.filters.language} />
-                                            <img style="display: none;" class="refresh_language" src={svg.refresh_filter} />
+                                        <div class="filters__select filters__select_direction_horizont">
+                                            {Variable.lang.text.country}
+                                        </div>
+                                        <div class="filters__checkboxes">
+                                            <div class="filters__checkbox">
+                                                <input
+                                                    checked={true}
+                                                    type="checkbox"
+                                                    id="common"
+                                                // required="required"
+                                                // onChange={async () => {
+                                                //     Static.filters.group.common = !Static.filters.group.common
+                                                //     Static.apiFilter = makeFilter(Static)
+                                                //     await fn.restApi.getUsers({ name: Static.nameRecords, filter: Static.apiFilter })
+                                                // }}
+                                                />
+                                                <label for="common">{Variable.lang.h.top_users}</label>
+                                            </div>
+                                            <div class="filters__checkbox">
+                                                <input
+                                                    checked={true}
+                                                    type="checkbox"
+                                                    id="content-makers"
+                                                // required="required"
+                                                // onChange={async () => {
+                                                //     Static.filters.group.content = !Static.filters.group.content
+                                                //     Static.apiFilter = makeFilter(Static)
+                                                //     await fn.restApi.getUsers({ name: Static.nameRecords, filter: Static.apiFilter })
+                                                // }}
+                                                />
+                                                <label for="content-makers">{Variable.lang.select.users_contentCreater}</label>
+                                            </div>
+                                            <div class="filters__checkbox">
+                                                <input
+                                                    checked={true}
+                                                    type="checkbox"
+                                                    id="specialists"
+                                                // required="required"
+                                                // onChange={async () => {
+                                                //     Static.filters.group.expert = !Static.filters.group.expert
+                                                //     Static.apiFilter = makeFilter(Static)
+                                                //     await fn.restApi.getUsers({ name: Static.nameRecords, filter: Static.apiFilter })
+                                                // }}
+                                                />
+                                                <label for="specialists">{Variable.lang.select.users_experts}</label>
+                                            </div>
+                                            <button
+                                                class="filters__button"
+                                                title={Variable.lang.a.resetFilter}
+                                            >
+                                                <img src={svg.reset_filter} width="30" height="30" />
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
+
                         </div>
                     </div>
                 </div>
