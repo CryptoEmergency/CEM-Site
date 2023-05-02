f1026071343c30625839885257447fd23681fa37
Crypto Emergency <89574207+CryptoEmergency@users.noreply.github.com>
2023-05-02 11:45:26 +0300

    Update about.js

--- src/navigation/about.js
+++ src/navigation/about.js
@@ -5,12 +5,14 @@ import {
   Variable,
   initReload,
   load,
-  CEM
 } from "@betarost/cemserver/cem.js";
-
+import svg from "@assets/svg/index.js";
+import images from "@assets/images/index.js";
 import Elements from "@src/elements/export.js";
 
-const { images, svg, fn, elements } = CEM
+import { Particles } from "@src/component/htmlElements/index.js";
+
+// import { AboutAnimation } from "@src/component/blocks/AboutAnimation";
 
 const team = [
   {
@@ -156,119 +158,6 @@ const start = function (data, ID) {
       src: svg["roadmap/turn-left7"],
     },
   ];
-  const arrSocials = [
-    {
-      link: "https://t.me/cryptoemergencychat",
-      icon: "telegram",
-      name: "Telegram",
-      lang: "ru"
-    },
-    {
-      link: "https://t.me/emergencycrypto",
-      icon: "telegram",
-      name: "Telegram",
-      lang: "gb"
-    },
-    {
-      link: "https://instagram.com/cryptoemergency",
-      icon: "instagram",
-      name: "Instagram",
-      lang: "ru"
-
-    },
-    {
-      link: "https://instagram.com/cryptoemergency",
-      icon: "instagram",
-      name: "Instagram",
-      lang: "gb"
-    },
-    {
-      link: "https://www.tiktok.com/@cryptoemergencyrussia",
-      icon: "tiktok_icon",
-      name: "Tiktok",
-      lang: "ru"
-    },
-    {
-      link: "https://vm.tiktok.com/ZSefExJrr",
-      icon: "tiktok_icon",
-      name: "Tiktok",
-      lang: "gb"
-    },
-    {
-      link: "https://youtube.com/channel/UCb9Fx-fN",
-      icon: "youtube_icon",
-      name: "Youtube",
-      lang: "ru"
-    },
-    {
-      link: "https://www.facebook.com/groups/crypt",
-      icon: "facebook_icon",
-      name: "Facebook",
-      lang: "gb"
-    },
-    {
-      link: "https://vk.com/club209389938",
-      icon: "vk_icon",
-      name: "Vk",
-      lang: "ru"
-    },
-    {
-      link: "https://twitter.com/cryptoemergency",
-      icon: "twitter",
-      name: "Twitter",
-      lang: "gb"
-    }
-  ];
-  const projectS = [{
-    link: "https://duma.network/",
-    icon: "logo-preload",
-    name: "Duma",
-    opis: "Инструмент для инвесторов. Исследования, аналитика, безопасный вклад любой суммы, разделение SAFT и продажа через платформу.",
-    images: true
-  },
-  {
-    link: "https://cemblockchain.com",
-    icon: "cem_logo",
-    name: "Blockchain",
-    opis: "Crypto Emergency — общедоступная блокчейн-платформа с открытым исходным кодом, низкими комиссиями и отличной поддержкой продуктов."
-  },
-  {
-
-    link: "https://cemscan.com",
-    icon: "cem_logo",
-    name: "Block Explorer",
-    opis: "Визуализации блоков, истории транзакций и показателей блокчейна Crypto Emergency.",
-  },
-  {
-    link: "https://cemwallet.com/",
-    icon: "wallet_icon",
-    name: "Cem Wallet",
-    opis: "CEM Wallet - официальный криптовалютный кошелек Crypto Emergency. Отправляйте и получайте CEM и многие другие криптовалюты с помощью мобильного приложения CEM Wallet"
-  }
-  ];
-  const sait_small = [{
-
-    link: "https://sberunity.ru/profile/879d88aa-4729-4788-a541-20cc8cc3cb14",
-    icon: "sber",
-    name: "Сбер Unity",
-    opis: "Платформу для взаимодействия инвесторов и стартапов Инвесторы могут оставлять запросы, а стартапы напрямую откликаться на них.",
-    //small: true
-  },
-  {
-    link: "https://chainlist.org/chain/193",
-    icon: "chainList",
-    name: "ChainList",
-    opis: "Chainlist — это список сетей EVM. Пользователи могут использовать эту информацию для подключения своих кошельков и поставщиков промежуточного программного обеспечения Web3 к соответствующему идентификатору цепочки и идентификатору сети для подключения к правильной цепочке."
-  },
-
-  {
-    link: "https://github.com/CryptoEmergency",
-    icon: "github_icon",
-    name: "Github",
-    opis: "крупнейший веб-сервис для хостинга IT-проектов и их совместной разработки.",
-    //small: true
-
-  }];
 
   load({
     ID,
@@ -277,185 +166,64 @@ const start = function (data, ID) {
         <Elements.page.MainContainer
           // class="c-aboutus"
           header={
-            <div class="c-aboutus__whowe c-whowe">
-              <div class="c-whowe__inner pt--36 ">
-                <img class="c-whowe__img" src={svg["background/about_us_vector-1"]} />
-                <h2 class="indexZ--3 mb--24">Crypto Emergency</h2>
-                <p>{Variable.lang.p.aboutObjective}</p>
-              </div>
-              <div class="c-whowe__bg"></div>
-            </div>
+            <Elements.page.Header
+              imgBack={svg["background/about_us_vector-1"]}
+              title="Crypto Emergency"
+              descriptions={Variable.lang.p.aboutObjective}
+              classBack="c-whowe__bg"
+            />
           }
         >
           {/* <div class="c-aboutus about_us_container c-main__body"> */}
-          <section class={["c-aboutus__content", "c-container"]}>
-            <section class={["c-aboutus__goals", "c-goals"]}>
-              <h2>{Variable.lang.h.our_goals}</h2>
 
+          {/* <Particles></Particles> */}
 
-
-
+          <Elements.page.Container
+            class={["c-aboutus__content", "c-container"]}
+            resetClass={true}
+          >
+            <Elements.page.Container
+              class={["c-aboutus__goals", "c-goals"]}
+              resetClass={true}
+              title={Variable.lang.h.our_goals}
+              backSeat={{
+                src: svg["background/about_us_vector-2"],
+                class: "c-whowe__img c-whowe__img--right",
+              }}
+            >
               <Elements.cards.Animated records={arrBlockCard} />
 
-              <elements.Accordeon records={arrAccordeon} />
+              <Elements.Accordeon records={arrAccordeon} />
 
               <Elements.page.Container
                 class={["team"]}
                 resetClass={true}
                 title={Variable.lang.h.our_team}
               >
-                <elements.sliderTeam records={team} />
+                <Elements.Team records={team} />
               </Elements.page.Container>
-              <img class="c-whowe__img c-whowe__img--right" src={svg["background/about_us_vector-2"]} />
-            </section>
+            </Elements.page.Container>
 
             <Elements.page.Container
               resetClass={true}
               title={<h2 class="mb--50">{Variable.lang.h.road_map}</h2>}
             >
-              <elements.Roadmap records={roadmap} />
+              <Elements.Roadmap records={roadmap} />
             </Elements.page.Container>
 
             <Elements.page.Container
               class={"indexZ--2"}
               resetClass={true}
-            //title={Variable.lang.h.our_projects}
+              title={Variable.lang.h.our_projects}
             >
-
-              {/* <div class="block-section social">
-                <h3 class="H3">Социальные сети</h3>
-                <div class="btns-wrap-1">
-                  {
-                    arrSocials.map((item, index) => {
-                      console.log('=c559f5=', item.images)
-                      return (
-                        <a href={item.link} class="btn-item" target="_blank">
-                          <img class="btn-item_img" src={!item.images ? svg[`train/${item.icon}`] : images[item.icon]} alt={item.name} />
-                          <span>{item.name}</span>
-                          <img class="btn-item_img-flag" src={`/assets/icons/flagsnew/${item.lang}.svg`} alt="Flag" />
-                        </a>
-
-
-
-                      )
-                    })
-                  }
-                </div>
-              </div> */}
-              {/* <div class="block-section-2">
-
-                <h4 class="H3">Наши проекты</h4>
-                <div class="btns-wrap-2">
-                  {
-                    projectS.map((item) => {
-                      console.log('=c559f5=', item.link)
-                      return (
-                        <a
-                          href={item.link}
-                          class="btn-item-2"
-                          target="_blank"
-                        >
-                          <span class="SP2">{item.name}
-
-                            <p class="p1">
-                              <img class="btn-item_img-2" src={!item.images ? svg[`train/${item.icon}`] : images[item.icon]} alt={item.name} />
-                              {item.opis}
-                            </p>
-                          </span>
-
-                        </a>
-                      )
-                    })
-                  }
-
-                </div>
-
-
-
-              </div> */}
-              {/* <div class="block-section-2">
-                <h4 class="H3">Ссылки</h4>
-                <div class="btns-wrap-2 btns-wrap-small">
-                  {sait_small.map((item) => {
-                    console.log('=c559f5=', item.link)
-                    return (
-                      <a class="btn-small btn-item-2" href={item.link} target="_blank">
-                        <img class="btn-item_img-2" src={!item.imeges ? svg[`train/${item.icon}`] : images[item.icon]}></img>
-                        <div class="decs">
-                          <h5 class="H5">{item.name}</h5>
-                          <p class="p1">
-
-                            {item.opis}</p>
-                        </div>
-                      </a>
-                    )
-                  })
-                  }
-
-                </div>
-              </div> */}
-              {/* <div class="block-section-3">
-
-                <h5 class="H3">Мобильные приложения</h5>
-                <div class="btns-wrap-3">
-
-
-                  <div class="btn-item-3">
-                    <h5 class="H5">Crypto Emergency
-                      <p class="P2">
-                        <img class="btn-item_img-3" src={svg['cem_logo']} alt="Telegram" />
-                        Crypto Emergency – крипто социальная сеть, основанная на собственном блокчейне, имеющая огромную экосистему из разнообразных продуктов.Здесь можно найти криптоэкспертов из любой страны, на любом языке и обучиться у них криптомастерству: что такое блокчейн, что такое криптовалюта, как это работает, как на этом зарабатывать, как создать свой криптостартап и получить другие советы.
-                      </p>
-                    </h5>
-                    <div class="apps-wrap">
-                      <a href="https://play.google.com/store/apps/details?id=com.cryptoemergency&hl=en&gl=US" class="go-apps" target="_blank">
-                        <img src={svg['googleplay']}></img>
-                      </a>
-                      <a href="https://apps.apple.com/ru/app/crypto-emergency/id1635628021" class="go-apps" target="_blank">
-                        <img src={svg['appstore']}></img>
-                      </a>
-                    </div>
-
-                  </div>
-
-                  <a href="https://play.google.com/store/apps/details?id=com.cemwallet&hl=en&gl=US" class="btn-item-3" target="_blank">
-                    <h5 class="H5">CEM Wallet
-                      <p class="P2">
-                        <img class="btn-item_img-3" src={svg['cem_logo']} alt="Telegram" />
-                        CEM Wallet - официальный криптовалютный кошелек Crypto Emergency. Отправляйте и получайте CEM и многие другие криптовалюты с помощью мобильного приложения CEM Wallet
-                      </p>
-                    </h5>
-                    <div class="apps-wrap">
-                      <a href="https://play.google.com/store/apps/details?id=com.cemwallet&hl=en&gl=US" class="go-apps" target="_blank">
-                        <img src={svg['googleplay']}></img>
-                      </a>
-                      <a href="https://apps.apple.com/ru/app/cem-wallet/id1637300554" class="go-apps" target="_blank">
-                        <img src={svg['appstore']}></img>
-                      </a>
-                    </div>
-                  </a>
-
-
-                </div>
-              </div> */}
-              {/* <Elements.projects.Project
+              <Elements.projects.Project
                 records={projects}
                 title={projects.title}
                 link={projects.link}
                 scr={projects.src}
-              /> */}
-
+              />
             </Elements.page.Container>
-            <Elements.page.Container
-              class={"c-aboutus__donats"}
-              resetClass={true}
-            >
-              <a href="https://www.donationalerts.com/r/crypto_emergency" rel="nofollow nooopener" target="_blank" class="c-button c-button--icon">
-                <img class="c-button__image" src={svg.donationalerts} width="37" height="43" />
-                <span class="c-button__text">{Variable.lang.span.supportProject}</span>
-              </a>
-            </Elements.page.Container>
-          </section>
+          </Elements.page.Container>
         </Elements.page.MainContainer>
       );
     },
